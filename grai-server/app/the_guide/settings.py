8e2454767c4338e3bb0504ea4a88394c0bda84ff
Edward Louth <edward@louths.com>
2022-10-27 17:59:16 +0100

    fix lint issues

--- grai-server/app/the_guide/settings.py
+++ grai-server/app/the_guide/settings.py
@@ -185,6 +185,6 @@ CSRF_COOKIE_SECURE = True
 
 CORS_ORIGIN_ALLOW_ALL = False
 CORS_ORIGIN_WHITELIST = (
-  'http://localhost:3000',
-  'http://localhost:8000',
+    "http://localhost:3000",
+    "http://localhost:8000",
 )
