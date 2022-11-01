67eca92bc58290ed0e0686d0ab69a7f5f8144549
Ian Eaves <ian.k.eaves@gmail.com>
2022-11-01 12:36:52 -0500

    test

--- grai-server/app/the_guide/settings.py
+++ grai-server/app/the_guide/settings.py
@@ -41,7 +41,7 @@ DATABASES = {
         "USER": config("DB_USER", default="grai"),
         "PASSWORD": config("DB_PASSWORD", default="grai"),
         "HOST": config("DB_HOST", default="localhost"),
-        "PORT": config("DB_PORT", default=5432),
+        "PORT": config("DB_PORT", default='5432'),
     }
 }
 
@@ -67,6 +67,10 @@ THIRD_PARTY_APPS = [
     "rest_framework.authtoken",
     "rest_framework_api_key",
     "health_check",
+    'health_check.db',
+    'health_check.cache',
+    'health_check.storage',
+    'health_check.contrib.migrations',
 ]
 
 THE_GUIDE_APPS = [
