3970b5b447723225e9401d85e41b63f29b4c535b
Ian Eaves <ian.k.eaves@gmail.com>
2022-11-10 00:59:49 -0600

    still not working

--- grai-server/app/the_guide/settings.py
+++ grai-server/app/the_guide/settings.py
@@ -118,7 +118,7 @@ MIDDLEWARE = [
     "whitenoise.middleware.WhiteNoiseMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
-    "django.middleware.csrf.CsrfViewMiddleware",
+    # "django.middleware.csrf.CsrfViewMiddleware",
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
