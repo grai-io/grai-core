9f99dec13f1c487aa3ed107b79a328645e9bec27
Edward Louth <edward@louths.com>
2022-11-16 10:26:20 +0000

    Add additional logging

--- grai-server/app/the_guide/settings.py
+++ grai-server/app/the_guide/settings.py
@@ -222,3 +222,22 @@ CSRF_COOKIE_SECURE = True
 
 # OpenApi
 # https://drf-spectacular.readthedocs.io/en/latest/settings.html
+
+LOGGING = {
+   'version': 1,
+   'disable_existing_loggers': False,
+   'handlers': {
+      'file': {
+         'level': 'DEBUG',
+         'class': 'logging.FileHandler',
+         'filename': '/tmp/debug.log',
+      },
+   },
+   'loggers': {
+      'django': {
+         'handlers': ['file'],
+         'level': 'DEBUG',
+         'propagate': True,
+      },
+   },
+}
