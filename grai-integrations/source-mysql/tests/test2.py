538e374b3307a8f2dcb7eaec0bb7a3b8fb4c17e9
Tony <xx@yy.com>
2022-11-25 16:40:54 +0000

    Remove test scripts

--- grai-integrations/source-mysql/tests/test2.py
+++
@@ -1,7 +0,0 @@
-from grai_source_mysql.base import update_server
-from grai_client.endpoints.v1.client import ClientV1
-
-client = ClientV1("localhost","8000")
-client.set_authentication_headers("null@grai.io","super_secret")
-
-update_server(client, dbname='db', user='docker', password='docker', namespace='jaffle_shop', host='localhost', port='3306')
