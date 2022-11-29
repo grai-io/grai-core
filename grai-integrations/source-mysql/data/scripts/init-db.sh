03b7049e6e28b6b6d316edfbe8b42d253104aea2
Ian Eaves <ian.k.eaves@gmail.com>
2022-11-28 21:35:09 -0600

    db-init

--- grai-integrations/source-mysql/data/scripts/init-db.sh
+++ grai-integrations/source-mysql/data/scripts/init-db.sh
@@ -1,4 +1,10 @@
-for f in `find '../schema' -type f -name '*.sql'`
+host="${DB_HOST:-localhost}"
+port="${DB_PORT:-3306}"
+user="${DB_USER:-grai}"
+password="${DB_PASSWORD:-grai}"
+database="${DB_DATABASE:-grai}"
+
+for file in `find '../schema' -type f -name '*.sql'`
   do
-    mysql -u grai -p grai -h localhost grai < $f
+    mysql -u $user -p $password -h $host $database < $file
  done
