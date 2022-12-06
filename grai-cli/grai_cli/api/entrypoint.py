2b1aedb7729afff90e847d639bf7cb89462a8d1c
Ian Eaves <ian.k.eaves@gmail.com>
2022-12-06 12:10:13 +0000

    typo

--- grai-cli/grai_cli/api/entrypoint.py
+++ grai-cli/grai_cli/api/entrypoint.py
@@ -7,4 +7,39 @@ from grai_cli.settings.cache import cache
 from grai_cli.utilities.telemetry import Telemetry
 from grai_cli.utilities.utilities import HAS_RICH, default_callback
 
-g
+
+def result_callback(*args, **kwargs):
+    ctx = click.get_current_context()
+    command_path = ctx.meta["command_path"]
+    if command_path and command_path[0] != "telemetry":
+        command = f"grai {' '.join(command_path)}"
+        Telemetry.capture(command)
+
+
+app = typer.Typer(
+    rich_markup_mode="rich",
+    invoke_without_command=True,
+    no_args_is_help=True,
+    help="Grai CLI",
+    pretty_exceptions_show_locals=False,
+    result_callback=result_callback,
+)
+
+
+@app.callback()
+def callback(
+    ctx: typer.Context,
+    telemetry: Optional[bool] = typer.Option(
+        None, show_default=False, help="Enable or disable telemetry"
+    ),
+):
+    """
+    Grai CLI
+    """
+
+    if telemetry is not None:
+        from grai_cli.settings.cache import cache
+
+        cache.set("telemetry_consent", telemetry)
+
+    default_callback(ctx)
