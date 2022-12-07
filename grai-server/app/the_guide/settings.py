b63522ea52adff9a6c452e053f5c5e1fb25cb5e9
Ian Eaves <ian.k.eaves@gmail.com>
2022-12-07 01:43:33 +0000

    3.11

--- grai-server/app/the_guide/settings.py
+++ grai-server/app/the_guide/settings.py
@@ -25,7 +25,15 @@ def clean_hosts(val):
 # SECURITY WARNING: keep the secret key used in production secret!
 
 
-SECRET_KEY = config("SECRET_KEY")  # Default secret_key generated in entrypoint.sh
+"""
+SECRET_KEY
+    The default secret key should only be needed if manage.py is invoked without entrypoint.sh
+    such as in the case of local tests
+"""
+
+SECRET_KEY = config(
+    "SECRET_KEY", "manual_tests"
+)  # Default secret_key generated in entrypoint.sh
 USER_ID = hashlib.md5(SECRET_KEY.encode()).hexdigest()
 DEBUG = config("DEBUG", default=False, cast=bool)
 TEMPLATE_DEBUG = config("TEMPLATE_DEBUG", default=DEBUG, cast=bool)
