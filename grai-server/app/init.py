25e34d999be852eb291ba489ca223c48185ff32b
Edward Louth <edward@louths.com>
2023-01-02 13:25:13 +0000

    Change initial user creation to always create workspace

--- grai-server/app/init.py
+++ grai-server/app/init.py
@@ -12,21 +12,17 @@ User = get_user_model()
 username = config("DJANGO_SUPERUSER_USERNAME", None)
 password = config("DJANGO_SUPERUSER_PASSWORD", None)
 email = config("DJANGO_SUPERUSER_EMAIL", None)
-workspace_name = config("DJANGO_SUPERUSER_WORKSPACE", None)
+workspace_name = config("DJANGO_SUPERUSER_WORKSPACE", "Default Workspace")
 
 if not username and email:
     username = email
-
-user = None
 
 if username and password and not User.objects.filter(is_superuser=True).exists():
     print(f"Creating superuser {username}")
     user = User.objects.create_superuser(username, password)
 
-if workspace_name and not Workspace.objects.filter(name=workspace_name).exists():
-    print(f"Creating workspace {workspace_name}")
-    workspace = Workspace.objects.create(name=workspace_name)
+    print(f"Get or creating workspace {workspace_name}")
+    workspace = Workspace.objects.get_or_create(name=workspace_name)
 
-    if user:
-        print(f"Creating membership")
-        Membership.objects.create(role="admin", user=user, workspace=workspace)
+    print(f"Creating membership")
+    Membership.objects.create(role="admin", user=user, workspace=workspace)
