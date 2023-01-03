c105418cb80a59163908820ed632b8f3d70b8944
Edward Louth <edward@louths.com>
2023-01-03 16:11:19 +0000

    Fix error handling

--- grai-server/app/connections/admin.py
+++ grai-server/app/connections/admin.py
@@ -1,7 +1,14 @@
 from django.contrib import admin
-
 from .models import Connector, Connection, Run
+
+
+class RunAdmin(admin.ModelAdmin):
+    list_filter = (
+        "status",
+        ("connection", admin.RelatedOnlyFieldListFilter),
+    )
+
 
 admin.site.register(Connector)
 admin.site.register(Connection)
-admin.site.register(Run)
+admin.site.register(Run, RunAdmin)
--- grai-server/app/connections/tasks.py
+++ grai-server/app/connections/tasks.py
@@ -4,13 +4,6 @@ from datetime import datetime
 
 
 @shared_task
-def add(x, y):
-    print("Hello task running")
-    print(x + y)
-    # return x + y
-
-
-@shared_task
 def run_update_server(runId):
     print(f"Task starting {runId}")
 
@@ -24,11 +17,26 @@ def run_update_server(runId):
 
     try:
         # update_server
+        connector = run.connection.connector
+
+        if connector.name == "postgres":
+            run_postgres()
+        else:
+            raise NoConnectorError("No connector found")
+
         run.status = "success"
-        run.finished = datetime.now()
+        run.finished_at = datetime.now()
         run.save()
     except Exception as e:
-        run.metadata = e
+        run.metadata = str(e)
         run.status = "error"
-        run.finished = datetime.now()
+        run.finished_at = datetime.now()
         run.save()
+
+
+def run_postgres():
+    pass
+
+
+class NoConnectorError(Exception):
+    """raise this when no connection is found"""
