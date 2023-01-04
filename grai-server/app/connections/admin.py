f165e8b877d7a50643b8729981bdbf5b4b65cf89
Edward Louth <edward@louths.com>
2023-01-04 15:48:22 +0000

    Admin panel improvements (#72)
    * Admin panel improvements
    
    * BE style fixes
    
    * review app comment fix
    
    * Fix final_name

--- grai-server/app/connections/admin.py
+++ grai-server/app/connections/admin.py
@@ -1,6 +1,35 @@
 from django.contrib import admin
-
 from .models import Connector, Connection
 
-admin.site.register(Connector)
-admin.site.register(Connection)
+
+class ConnectorAdmin(admin.ModelAdmin):
+    list_display = ("id", "name", "is_active")
+
+    search_fields = ["id", "name"]
+
+    list_filter = ("is_active",)
+
+
+class ConnectionAdmin(admin.ModelAdmin):
+    list_display = (
+        "id",
+        "name",
+        "connector",
+        "workspace",
+        "namespace",
+        "is_active",
+        "created_by",
+    )
+
+    search_fields = ["id", "namespace", "name"]
+    list_filter = (
+        ("connector", admin.RelatedOnlyFieldListFilter),
+        ("workspace", admin.RelatedOnlyFieldListFilter),
+        ("created_by", admin.RelatedOnlyFieldListFilter),
+        "namespace",
+        "is_active",
+    )
+
+
+admin.site.register(Connector, ConnectorAdmin)
+admin.site.register(Connection, ConnectionAdmin)
