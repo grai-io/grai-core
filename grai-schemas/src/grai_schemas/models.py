b8a5babd3e3d39d7c652dfdcdf08cb9bb499e80d
Ian Eaves <ian.k.eaves@gmail.com>
2023-01-09 15:31:29 -0600

    pseudo implementation

---
+++ grai-schemas/src/grai_schemas/models.py
@@ -0,0 +1,45 @@
+from enum import Enum
+from typing import Any, Literal, Optional
+
+from pydantic import BaseModel
+
+
+class NodeTypes(Enum):
+    table = "Table"
+    column = "Column"
+
+
+class SourceType(Enum):
+    database = "SQL"
+
+
+class DefaultValue(BaseModel):
+    has_default_value: bool
+    data_type: Optional[str]
+    default_value: Optional[Any]
+
+
+class ColumnAttributes(BaseModel):
+    data_type: str  # This will need to be standardized
+    default_value: DefaultValue = DefaultValue(has_default_value=False)
+    is_nullable: Optional[bool]
+    is_primary_key: Optional[bool]
+
+
+class TableAttributes(BaseModel):
+    pass
+
+
+class GraiNodeMetadata(BaseModel):
+    node_type: NodeTypes
+    node_attributes: Any
+
+
+class Column(GraiNodeMetadata):
+    node_type: Literal["Column"] = "Column"
+    node_attributes: ColumnAttributes
+
+
+class Table(GraiNodeMetadata):
+    node_type: Literal["Table"] = "Table"
+    node_attributes: TableAttributes
---
+++ grai-schemas/tests/test_base.py
@@ -0,0 +1,2 @@
+def test_import():
+    import grai_schemas as gs
