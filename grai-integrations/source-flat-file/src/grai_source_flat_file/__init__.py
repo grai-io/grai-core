01eb6d23fc2964a3d24c16380ad4c99222ab9bd6
Ian Eaves <ian.k.eaves@gmail.com>
2023-01-11 16:47:41 -0600

    flat-file

--- grai-integrations/source-flat-file/src/grai_source_flat_file/__init__.py
+++ grai-integrations/source-flat-file/src/grai_source_flat_file/__init__.py
@@ -1,1 +1,2 @@
-from grai_source_flat_file import adapters, base, loader, models
+from grai_source_flat_file import adapters, base, loader, models, package_definitions
+from grai_source_flat_file.package_definitions import config
