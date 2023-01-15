0fa19a61aebe38698d9f15b987cdd0bec9570ba9
Edward <edward@louths.org.uk>
2023-01-15 12:03:29 +0000

    basic start to datadiff

--- grai-server/app/api/queries.py
+++ grai-server/app/api/queries.py
@@ -2,10 +2,12 @@ import typing
 
 import strawberry
 from strawberry.types import Info
+from strawberry.scalars import JSON
 from strawberry_django_plus import gql
 
 from api.types import Connector, User, Workspace
 from workspaces.models import Workspace as WorkspaceModel
+from lineage.models import Node as NodeModel
 
 from .common import IsAuthenticated, get_user
 
@@ -33,6 +35,33 @@ def get_profile(info: Info) -> User:
     return get_user(info)
 
 
+def data_diff(
+    info: Info,
+    # workspaceId: strawberry.ID,
+    # table1Id: strawberry.ID,
+    # table2Id: strawberry.ID,
+) -> JSON:
+    import logging
+
+    logging.basicConfig(level=logging.INFO)
+
+    from data_diff import connect_to_table, diff_tables
+
+    # table1Model = NodeModel.objects.get(pk=table1Id)
+    # table2Model = NodeModel.objects.get(pk=table2Id)
+
+    table1 = connect_to_table(
+        "postgresql://grai:grai@localhost:5432/grai", "public.lineage_node", "id"
+    )
+    table2 = connect_to_table(
+        "postgresql://grai:grai@localhost:5432/grai",
+        "public.lineage_edge",
+        "id",
+    )
+
+    return dict(diff_tables(table1, table2))
+
+
 @gql.type
 class Query:
     workspaces: typing.List[Workspace] = strawberry.django.field(
@@ -46,4 +75,7 @@ class Query:
     )
     profile: User = strawberry.django.field(
         resolver=get_profile, permission_classes=[IsAuthenticated]
+    )
+    data_diff: JSON = strawberry.django.field(
+        resolver=data_diff, permission_classes=[IsAuthenticated]
     )
