93c6a9dd53738c12936ab29e19d70171abd53a27
Ian Eaves <ian.k.eaves@gmail.com>
2023-01-17 15:02:27 -0600

    removed dbt

--- grai-graph/tests/conftest.py
+++ grai-graph/tests/conftest.py
@@ -1,6 +1,4 @@
 import pytest
-from grai_source_dbt.base import get_nodes_and_edges
-from grai_source_dbt.utils import get_manifest_file
 
 from grai_graph import graph
 from grai_graph.test_utils import TestNodeObj
@@ -8,18 +6,20 @@ from grai_graph.test_utils import TestNodeObj
 TestNodeObj.__test__ = False
 
 
-@pytest.fixture
-def jaffle_shop_manifest():
-    return get_manifest_file("jaffle_shop")
-
-
-@pytest.fixture
-def jaffle_nodes_and_edges(jaffle_shop_manifest):
-    return get_nodes_and_edges(jaffle_shop_manifest)
-
-
-@pytest.fixture
-def jaffle_graph(jaffle_nodes_and_edges):
-    nodes, edges = jaffle_nodes_and_edges
-    jaffle_graph = graph.build_graph(nodes, edges, "v1")
-    return jaffle_graph
+# @pytest.fixture
+# def jaffle_shop_manifest():
+#     from grai_source_dbt.utils import get_manifest_file
+#     return get_manifest_file("jaffle_shop")
+#
+#
+# @pytest.fixture
+# def jaffle_nodes_and_edges(jaffle_shop_manifest):
+#     from grai_source_dbt.base import get_nodes_and_edges
+#     return get_nodes_and_edges(jaffle_shop_manifest)
+#
+#
+# @pytest.fixture
+# def jaffle_graph(jaffle_nodes_and_edges):
+#     nodes, edges = jaffle_nodes_and_edges
+#     jaffle_graph = graph.build_graph(nodes, edges, "v1")
+#     return jaffle_graph
