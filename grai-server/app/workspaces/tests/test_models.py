f7e0767483a0a6d2adeccb258f719667f613a160
Edward <edward@louths.org.uk>
2023-01-19 11:30:29 +0000

    Add test coverage

--- grai-server/app/workspaces/tests/test_models.py
+++ grai-server/app/workspaces/tests/test_models.py
@@ -8,12 +8,25 @@ from workspaces.models import Workspace, Organisation
 
 
 @pytest.mark.django_db
+def test_organisation_created():
+    organisation = Organisation.objects.create(name="O1")
+
+    assert organisation.id == uuid.UUID(str(organisation.id))
+    assert organisation.name == "O1"
+    assert str(organisation) == "O1"
+    assert isinstance(organisation.created_at, datetime.datetime)
+    assert isinstance(organisation.updated_at, datetime.datetime)
+
+
+@pytest.mark.django_db
 def test_workspace_created():
     organisation = Organisation.objects.create(name="O1")
     workspace = Workspace.objects.create(name="abc", organisation=organisation)
 
     assert workspace.id == uuid.UUID(str(workspace.id))
     assert workspace.name == "abc"
+    assert str(workspace) == "abc"
+    assert workspace.ref() == "O1/abc"
     assert isinstance(workspace.created_at, datetime.datetime)
     assert isinstance(workspace.updated_at, datetime.datetime)
 
