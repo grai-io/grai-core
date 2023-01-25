f4e9eeb562f7fa4642af8e674d6d5584b9324617
Edward Louth <edward@louths.com>
2023-01-25 10:06:00 +0000

    Add organisations to admin

--- grai-server/app/workspaces/admin.py
+++ grai-server/app/workspaces/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
 
-from .models import Membership, Workspace, WorkspaceAPIKey
+from .models import Membership, Organisation, Workspace, WorkspaceAPIKey
 
+admin.site.register(Organisation)
 admin.site.register(Workspace)
 admin.site.register(Membership)
 admin.site.register(WorkspaceAPIKey)
