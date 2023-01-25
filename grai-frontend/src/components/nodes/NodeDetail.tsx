12e4c2a9f48d8b7afcc1664a8063f4437cb32e1b
Edward Louth <edward@louths.com>
2023-01-25 16:12:28 +0000

    Additional node details

--- grai-frontend/src/components/nodes/NodeDetail.tsx
+++ grai-frontend/src/components/nodes/NodeDetail.tsx
@@ -4,11 +4,24 @@ import NodeDetailRow from "./NodeDetailRow"
 
 interface Node {
   name: string
+  namespace: string
+  data_source: string
   metadata: any
 }
 
 type NodeDetailProps = {
   node: Node
+}
+
+const valueToString = (value: any): string => {
+  switch (typeof value) {
+    case "string":
+      return value
+    case "boolean":
+      return value ? "yes" : "no"
+  }
+
+  return JSON.stringify(value)
 }
 
 const NodeDetail: React.FC<NodeDetailProps> = ({ node }) => (
@@ -19,12 +32,10 @@ const NodeDetail: React.FC<NodeDetailProps> = ({ node }) => (
         <NodeDetailRow label="# of rows" />
         <NodeDetailRow label="# of columns" />
         <NodeDetailRow label="Name" value={node.name} />
+        <NodeDetailRow label="Namespace" value={node.namespace} />
+        <NodeDetailRow label="Data Source" value={node.data_source} />
         {Object.entries(node.metadata).map(([key, value]) => (
-          <NodeDetailRow
-            key={key}
-            label={key}
-            value={typeof value === "string" ? value : value ? "yes" : "no"}
-          />
+          <NodeDetailRow key={key} label={key} value={valueToString(value)} />
         ))}
       </TableBody>
     </Table>
