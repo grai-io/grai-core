35528ae76cbf1a683d45eff2ec14a9c6ab718fc3
Ian Eaves <ian.k.eaves@gmail.com>
2023-01-26 18:53:29 -0600

    sorting scripts

--- grai-integrations/source-mysql/data/scripts/init-db.sh
+++ grai-integrations/source-mysql/data/scripts/init-db.sh
@@ -4,7 +4,7 @@ user="${DB_USER:-grai}"
 password="${DB_PASSWORD:-grai}"
 database="${DB_DATABASE:-grai}"
 
-for file in `find ../schema -type f -name *.sql`
+for file in `find ../schema -type f -name *.sql | sort`
   do
     echo "Executing $file"
     mysql -u $user --password=$password -h $host --protocol=TCP $database < $file
