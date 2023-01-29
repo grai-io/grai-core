0d37a1fb4d66aaf84f18b606b72b322e65346bf4
Edward Louth <edward@louths.com>
2023-01-29 11:51:03 +0000

    Fix cache eviction issue

--- grai-frontend/src/helpers/cache.ts
+++ grai-frontend/src/helpers/cache.ts
@@ -13,6 +13,20 @@ export const clearWorkspace = (cache: ApolloCache<any>, id: string) => {
       id,
       __typename: "Workspace",
     }),
+    fieldName: "tables",
+  })
+  cache.evict({
+    id: cache.identify({
+      id,
+      __typename: "Workspace",
+    }),
     fieldName: "edges",
+  })
+  cache.evict({
+    id: cache.identify({
+      id,
+      __typename: "Workspace",
+    }),
+    fieldName: "other_edges",
   })
 }
