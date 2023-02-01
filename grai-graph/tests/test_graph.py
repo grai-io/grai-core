1a9dc40cbded5dcf22d494f3c740c695dd936856
Ian Eaves <ian.k.eaves@gmail.com>
2023-02-01 13:36:03 -0600

    moar tests

--- grai-graph/tests/test_graph.py
+++ grai-graph/tests/test_graph.py
@@ -7,7 +7,11 @@ from grai_client.testing.schema import (
     mock_v1_edge_and_nodes,
     mock_v1_node,
 )
-from grai_schemas.v1.metadata.edges import ColumnToColumnAttributes
+from grai_schemas.v1.metadata.edges import (
+    ColumnToColumnAttributes,
+    TableToColumnAttributes,
+    TableToTableAttributes,
+)
 
 from grai_graph import graph
 from grai_graph.utils import (
@@ -49,6 +53,28 @@ class TestUniqueness(unittest.TestCase):
             return wraps
 
         return inner
+
+    @get_nodes(n=2)
+    def test_table_to_column_information(self, a, b):
+        """We don't know anything about node nullableness to evaluate failure or success"""
+        mock_structure = {
+            a: [("b", TableToColumnAttributes())],
+            b: [],
+        }
+        G = get_analysis_from_map(mock_structure)
+        results = G.test_unique_violations(name="a", namespace=DEFAULT_NAMESPACE, expects_unique=True)
+        assert len(results) == 0
+
+    @get_nodes(n=2)
+    def test_table_to_table_information(self, a, b):
+        """We don't know anything about node nullableness to evaluate failure or success"""
+        mock_structure = {
+            a: [("b", TableToTableAttributes())],
+            b: [],
+        }
+        G = get_analysis_from_map(mock_structure)
+        results = G.test_unique_violations(name="a", namespace=DEFAULT_NAMESPACE, expects_unique=True)
+        assert len(results) == 0
 
     @get_nodes(n=3)
     def test_no_node_information(self, a, b, c):
@@ -133,6 +159,28 @@ class TestNullable(unittest.TestCase):
             return wraps
 
         return inner
+
+    @get_nodes(n=2)
+    def test_table_to_column_information(self, a, b):
+        """We don't know anything about node nullableness to evaluate failure or success"""
+        mock_structure = {
+            a: [("b", TableToColumnAttributes())],
+            b: [],
+        }
+        G = get_analysis_from_map(mock_structure)
+        results = G.test_nullable_violations(name="a", namespace=DEFAULT_NAMESPACE, is_nullable=True)
+        assert len(results) == 0
+
+    @get_nodes(n=2)
+    def test_table_to_table_information(self, a, b):
+        """We don't know anything about node nullableness to evaluate failure or success"""
+        mock_structure = {
+            a: [("b", TableToTableAttributes())],
+            b: [],
+        }
+        G = get_analysis_from_map(mock_structure)
+        results = G.test_nullable_violations(name="a", namespace=DEFAULT_NAMESPACE, is_nullable=True)
+        assert len(results) == 0
 
     @get_nodes(n=3)
     def test_no_node_information(self, a, b, c):
@@ -231,6 +279,28 @@ class TestDataType(unittest.TestCase):
             return wraps
 
         return inner
+
+    @get_nodes(n=2)
+    def test_table_to_column_information(self, a, b):
+        """We don't know anything about node nullableness to evaluate failure or success"""
+        mock_structure = {
+            a: [("b", TableToColumnAttributes())],
+            b: [],
+        }
+        G = get_analysis_from_map(mock_structure)
+        results = G.test_type_change(name="a", namespace=DEFAULT_NAMESPACE, new_type="bool")
+        assert len(results) == 0
+
+    @get_nodes(n=2)
+    def test_table_to_table_information(self, a, b):
+        """We don't know anything about node nullableness to evaluate failure or success"""
+        mock_structure = {
+            a: [("b", TableToTableAttributes())],
+            b: [],
+        }
+        G = get_analysis_from_map(mock_structure)
+        results = G.test_type_change(name="a", namespace=DEFAULT_NAMESPACE, new_type="bool")
+        assert len(results) == 0
 
     @get_nodes(n=3)
     def test_no_node_information(self, a, b, c):
