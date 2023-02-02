f40a8ea360a0a9466c0eee65cb59f72bd6e4087b
Edward Louth <edward@louths.com>
2023-02-02 10:17:05 +0000

    Improved edge error display

---
+++ grai-frontend/src/components/graph/ErrorEdge.tsx
@@ -0,0 +1,102 @@
+import { ErrorOutline } from "@mui/icons-material"
+import {
+  Alert,
+  AlertTitle,
+  Box,
+  Divider,
+  Stack,
+  Typography,
+} from "@mui/material"
+import React, { useState } from "react"
+import { EdgeProps, getBezierPath, EdgeLabelRenderer } from "reactflow"
+
+interface Error {
+  message: string
+}
+
+export type ErrorData = {
+  errors?: Error[]
+}
+
+const ErrorEdge: React.FC<EdgeProps<ErrorData>> = ({
+  id,
+  sourceX,
+  sourceY,
+  targetX,
+  targetY,
+  sourcePosition,
+  targetPosition,
+  data,
+}) => {
+  const [edgePath, labelX, labelY] = getBezierPath({
+    sourceX,
+    sourceY,
+    sourcePosition,
+    targetX,
+    targetY,
+    targetPosition,
+  })
+
+  const [expand, setExpand] = useState(false)
+
+  const toggleExpand = () => {
+    console.log("toggle expand")
+    setExpand(!expand)
+  }
+
+  return (
+    <>
+      <path id={id} className="react-flow__edge-path" d={edgePath} />
+      <EdgeLabelRenderer>
+        <Alert
+          severity="error"
+          icon={false}
+          onClick={toggleExpand}
+          sx={{
+            pointerEvents: "all",
+            position: "absolute",
+            transform: `translate(-50%, -50%) translate(${labelX}px,${labelY}px)`,
+            // borderColor: theme => theme.palette.error.main,
+            // borderWidth: 1,
+            // borderStyle: "solid",
+            // backgroundColor: "white",
+            // color: theme => theme.palette.error.contrastText,
+            // padding: 1,
+            // borderRadius: 1,
+            // fontSize: 12,
+            // fontWeight: 700,
+            zIndex: 20,
+            width: 225,
+            overflowWrap: "break-word",
+          }}
+          className="nodrag nopan"
+        >
+          <Box sx={{ display: "flex" }}>
+            <ErrorOutline sx={{ mr: 1 }} />
+            <AlertTitle sx={{ mt: 0.1 }}>Errors</AlertTitle>
+          </Box>
+          {expand && (
+            <>
+              <Divider
+                sx={{
+                  mt: 0.5,
+                  mb: 1,
+                  borderColor: "rgba(95, 33, 32, 0.25)",
+                }}
+              />
+              <Stack spacing={1}>
+                {data?.errors?.map((error, index) => (
+                  <Typography variant="body2" key={index}>
+                    {error.message}
+                  </Typography>
+                ))}
+              </Stack>
+            </>
+          )}
+        </Alert>
+      </EdgeLabelRenderer>
+    </>
+  )
+}
+
+export default ErrorEdge
