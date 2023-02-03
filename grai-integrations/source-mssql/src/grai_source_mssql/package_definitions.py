2b859e7171aad98c5ea05199cfccb704d7355211
Ian Eaves <ian.k.eaves@gmail.com>
2023-02-03 13:01:24 -0600

    remove mssql

--- grai-integrations/source-mssql/src/grai_source_mssql/package_definitions.py
+++
@@ -1,9 +0,0 @@
-from grai_schemas.generics import PackageConfig
-
-
-class Config(PackageConfig):
-    integration_name = "grai-source-mssql"
-    metadata_id = "grai_source_mssql"
-
-
-config = Config()
