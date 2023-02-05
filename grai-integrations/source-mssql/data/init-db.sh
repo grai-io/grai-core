b85a3987d2830443b920af83260c2be4d10cf79a
Ian Eaves <ian.k.eaves@gmail.com>
2023-02-05 12:28:53 -0600

    run

--- grai-integrations/source-mssql/data/init-db.sh
+++ grai-integrations/source-mssql/data/init-db.sh
@@ -1,8 +1,10 @@
+#!/usr/bin/env bash
 
 sleep_time="${SLEEP_TIME:-30}"
+script_dir=$(dirname -- "$0")
 
 sleep $sleep_time
-for file in /init/schema/*.sql
+for file in $(find $script_dir -type f -name '*.sql' | sort)
    do
      echo "Executing init script $file"
      /opt/mssql-tools/bin/sqlcmd -U sa -P $MSSQL_SA_PASSWORD -l 30 -e -i $file
