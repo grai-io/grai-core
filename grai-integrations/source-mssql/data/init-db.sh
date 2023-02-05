1bd264628e4f8d24fa6ba02f53b007b7aa1599be
Ian Eaves <ian.k.eaves@gmail.com>
2023-02-05 17:14:59 -0600

    where are the scripts

--- grai-integrations/source-mssql/data/init-db.sh
+++ grai-integrations/source-mssql/data/init-db.sh
@@ -31,6 +31,8 @@ echo "Database ready"
 
 /opt/mssql-tools/bin/sqlcmd -U sa -P $MSSQL_SA_PASSWORD -l 30 -e -i 001.sql
 echo "da fuq"
+echo $(pwd)
+echo $(ls)
 for file in $(find . -type f -name '*.sql' | sort)
    do
      echo "Executing init script $file"
