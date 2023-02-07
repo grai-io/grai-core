626a85894f5e0130d15d52f12f12e5e9f8f8a976
Edward Louth <edward@louths.com>
2023-02-07 14:35:35 +0000

    Add runs to rest api

--- grai-server/app/connections/urls.py
+++ grai-server/app/connections/urls.py
@@ -2,12 +2,14 @@ from django.urls import path
 
 from rest_framework import routers
 
-from .views import ConnectionViewSet, ConnectorViewSet
+from .views import ConnectionViewSet, ConnectorViewSet, RunViewSet
 
 app_name = "connections"
 
 router = routers.SimpleRouter()
 router.register("connections", ConnectionViewSet, basename="connections")
 router.register("connectors", ConnectorViewSet, basename="connectors")
+router.register("runs", RunViewSet, basename="runs")
+
 
 urlpatterns = router.urls
