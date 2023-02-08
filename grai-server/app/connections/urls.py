ebdcb7f7cf6f493c2386f310cbcf337af1c371b0
Edward Louth <edward@louths.com>
2023-02-08 14:13:13 +0000

    Add action parameter to create_run

--- grai-server/app/connections/urls.py
+++ grai-server/app/connections/urls.py
@@ -22,62 +22,70 @@ router.register("connectors", ConnectorViewSet, basename="connectors")
 router.register("runs", RunViewSet, basename="runs")
 
 
-@api_view(["POST"])
-@permission_classes([(HasWorkspaceAPIKey | IsAuthenticated) & Multitenant])
-def create_run(request):
-    connector = None
-    connection = None
-    trigger = None
-
+def get_connection(request) -> Connection:
     connection_id = request.POST.get("connection_id")
 
     if connection_id is not None:
-        connection = Connection.objects.get(id=connection_id)
-        connector = connection.connector
+        return Connection.objects.get(id=connection_id)
 
-    else:
-        connector_name = request.POST.get("connector")
+    connector_name = request.POST.get("connector")
 
-        if connector_name is None:
-            raise Exception("You must provide a connector")
+    if connector_name is None:
+        raise Exception("You must provide a connector or connection_id")
 
-        connector = Connector.objects.get(name=connector_name)
+    connector = Connector.objects.get(name=connector_name)
 
-        name = f"{connector.name} {uuid.uuid4()}"
-        namespace = request.POST.get("namespace", "default")
-        metadata = request.POST.get("metadata")
-        secrets = request.POST.get("secrets")
+    name = f"{connector.name} {uuid.uuid4()}"
+    namespace = request.POST.get("namespace", "default")
+    metadata = request.POST.get("metadata")
+    secrets = request.POST.get("secrets")
 
-        connection = Connection.objects.create(
-            connector=connector,
-            name=name,
-            namespace=namespace,
-            is_active=True,
-            temp=True,
-            metadata=json.loads(metadata) if metadata else None,
-            secrets=json.loads(secrets) if secrets else None,
-        )
+    connection = Connection.objects.create(
+        connector=connector,
+        name=name,
+        namespace=namespace,
+        is_active=True,
+        temp=True,
+        metadata=json.loads(metadata) if metadata else None,
+        secrets=json.loads(secrets) if secrets else None,
+    )
 
+    return connection
+
+
+def get_trigger(request):
     github_installation_id = request.POST.get("github_installation_id")
-    if github_installation_id:
-        from .github import Github
 
-        git_owner = request.POST.get("git_owner")
-        git_repo = request.POST.get("git_repo")
-        git_head_sha = request.POST.get("git_head_sha")
+    if github_installation_id is None:
+        return None
 
-        github = Github(owner=git_owner, repo=git_repo, installation_id=github_installation_id)
-        check = github.create_check(head_sha=git_head_sha)
+    from .github import Github
 
-        trigger = {
-            "installation_id": github_installation_id,
-            "owner": git_owner,
-            "repo": git_repo,
-            "head_sha": git_head_sha,
-            "check_id": check.id,
-        }
+    git_owner = request.POST.get("git_owner")
+    git_repo = request.POST.get("git_repo")
+    git_head_sha = request.POST.get("git_head_sha")
 
-    run = Run.objects.create(connection=connection, status="queued", trigger=trigger)
+    github = Github(owner=git_owner, repo=git_repo, installation_id=github_installation_id)
+    check = github.create_check(head_sha=git_head_sha)
+
+    return {
+        "installation_id": github_installation_id,
+        "owner": git_owner,
+        "repo": git_repo,
+        "head_sha": git_head_sha,
+        "check_id": check.id,
+    }
+
+
+@api_view(["POST"])
+@permission_classes([(HasWorkspaceAPIKey | IsAuthenticated) & Multitenant])
+def create_run(request):
+    connection = get_connection(request)
+    trigger = get_trigger(request)
+
+    action = request.POST.get("action", "tests")
+
+    run = Run.objects.create(connection=connection, status="queued", trigger=trigger, action=action)
 
     run_update_server.delay(run.id)
 
