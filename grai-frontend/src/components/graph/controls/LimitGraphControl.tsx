011247ba6dda91d44dc86906e3919898516503c5
Edward Louth <edward@louths.com>
2023-02-12 17:00:20 +0000

    Fix limit graph size

---
+++ grai-frontend/src/components/graph/controls/LimitGraphControl.tsx
@@ -0,0 +1,34 @@
+import { Box, Button, Checkbox, Typography } from "@mui/material"
+import React from "react"
+
+type LimitGraphControlProps = {
+  value: boolean
+  onChange: (value: boolean) => void
+}
+
+const LimitGraphControl: React.FC<LimitGraphControlProps> = ({
+  value,
+  onChange,
+}) => (
+  <Box>
+    <Button
+      variant="outlined"
+      disableElevation
+      onClick={() => onChange(!value)}
+      sx={{
+        backgroundColor: "white",
+        borderColor: "rgba(0,0,0,0.23)",
+        color: "rgba(0, 0, 0, 0.6)",
+        pl: 0.5,
+        pr: 1.5,
+        minWidth: 0,
+        height: 40,
+      }}
+    >
+      <Checkbox sx={{ ml: 0.5, mr: 1, p: 0 }} size="small" checked={value} />
+      <Typography>Limit Graph</Typography>
+    </Button>
+  </Box>
+)
+
+export default LimitGraphControl
