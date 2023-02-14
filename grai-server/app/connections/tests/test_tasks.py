464c1ccd8139b93185a6104627221a14e86ab1b1
Edward Louth <edward@louths.com>
2023-02-14 19:16:25 +0000

    Add tests

--- grai-server/app/connections/tests/test_tasks.py
+++ grai-server/app/connections/tests/test_tasks.py
@@ -41,6 +41,9 @@ def test_mssql_connector():
 def test_bigquery_connector():
     return Connector.objects.create(name=Connector.BIGQUERY)
 
+@pytest.fixture
+def test_fivetran_connector():
+    return Connector.objects.create(name=Connector.FIVETRAN)
 
 @pytest.fixture
 def test_dbt_connector():
@@ -117,6 +120,50 @@ class TestUpdateServer:
             RunFile.objects.create(run=run, file=file)
 
             run_update_server(str(run.id))
+
+    def test_run_update_server_fivetran(self, test_workspace, test_fivetran_connector, mocker):
+        mocker.patch("grai_source_fivetran.loader.FivetranConnector")
+        mock = mocker.patch("grai_source_fivetran.base.get_nodes_and_edges")
+        mock.return_value = [[], []]
+        
+        connection = Connection.objects.create(
+            name="C1",
+            connector=test_fivetran_connector,
+            workspace=test_workspace,
+            metadata={
+                "api_key": "abc123"
+            },
+            secrets={
+                "api_secret": "abc123"
+            },
+        )
+
+        run = Run.objects.create(connection=connection, workspace=test_workspace)
+
+        run_update_server(str(run.id))
+
+    def test_run_update_server_fivetran_extras(self, test_workspace, test_fivetran_connector, mocker):
+        mocker.patch("grai_source_fivetran.loader.FivetranConnector")
+        mock = mocker.patch("grai_source_fivetran.base.get_nodes_and_edges")
+        mock.return_value = [[], []]
+        
+        connection = Connection.objects.create(
+            name="C1",
+            connector=test_fivetran_connector,
+            workspace=test_workspace,
+            metadata={
+                "api_key": "abc123",
+                "endpoint": "https://grai.io",
+                "limit": "10",
+            },
+            secrets={
+                "api_secret": "abc123"
+            },
+        )
+
+        run = Run.objects.create(connection=connection, workspace=test_workspace)
+
+        run_update_server(str(run.id))
 
     def test_run_update_server_yaml_file(self, test_workspace, test_yaml_file_connector):
         Node.objects.create(workspace=test_workspace, namespace="default", name="table1")
