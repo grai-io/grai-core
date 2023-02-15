4d61617c59227f60c3483ca348805af671a46f12
Edward Louth <edward@louths.com>
2023-02-15 10:11:52 +0000

    Fix lint

--- grai-server/app/connections/migrations/0017_merge_0010_alter_connector_name_0016_run_commit.py
+++ grai-server/app/connections/migrations/0017_merge_0010_alter_connector_name_0016_run_commit.py
@@ -4,7 +4,6 @@ from django.db import migrations
 
 
 class Migration(migrations.Migration):
-
     dependencies = [
         ("connections", "0010_alter_connector_name"),
         ("connections", "0016_run_commit"),
