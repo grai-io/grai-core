1a9bd1ca434ccc2c5ebdbb4b43448583a3b52695
Edward Louth <edward@louths.com>
2023-02-16 18:13:06 +0000

    Fix migration issue

--- grai-server/app/connections/migrations/0011_remove_run_connector_connection_temp_connector_slug_and_more.py
+++ grai-server/app/connections/migrations/0011_remove_run_connector_connection_temp_connector_slug_and_more.py
@@ -2,12 +2,48 @@
 
 import uuid
 
-import django.db.models.deletion
-import django_multitenant.fields
 from django.db import migrations, models
+from django_multitenant.mixins import TenantModelMixin
+
+
+def monkey_patch_multitenant() -> None:
+    def __setattr__(self, attrname, val):
+        if (
+            attrname in ("tenant_id", "tenant")
+            and not self._state.adding
+            and val
+            and self.tenant_value
+            and val != self.tenant_value
+            and val != self.tenant_object
+        ):
+            self._try_update_tenant = True
+
+        return super(TenantModelMixin, self).__setattr__(attrname, val)
+
+    TenantModelMixin.__setattr__ = __setattr__
+
+    @property
+    def tenant_field(self):
+        return "tenant_id"
+
+    TenantModelMixin.tenant_field = tenant_field
+
+    @property
+    def tenant_value(self):
+        return self.tenant_id
+
+    TenantModelMixin.tenant_value = tenant_value
+
+    @property
+    def tenant_object(self):
+        return self.tenant
+
+    TenantModelMixin.tenant_object = tenant_object
 
 
 def forwards_func(apps, schema_editor):
+    monkey_patch_multitenant()
+
     # We get the model from the versioned app registry;
     # if we directly import it, it'll be the wrong version
     Run = apps.get_model("connections", "Run")
@@ -36,15 +72,15 @@ class Migration(migrations.Migration):
     ]
 
     operations = [
-        migrations.RunPython(forwards_func),
-        migrations.RemoveField(
-            model_name="run",
-            name="connector",
-        ),
         migrations.AddField(
             model_name="connection",
             name="temp",
             field=models.BooleanField(default=False),
+        ),
+        migrations.RunPython(forwards_func),
+        migrations.RemoveField(
+            model_name="run",
+            name="connector",
         ),
         migrations.AddField(
             model_name="connector",
