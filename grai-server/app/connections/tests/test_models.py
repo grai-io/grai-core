039e4000ed9825e23fee66ba4317b2d612072418
Edward Louth <edward@louths.com>
2023-02-19 17:35:54 +0000

    Fix for clearing schedule (#209)
    * Fix for clearing schedule
    
    * Add tests
    
    * Better tests

--- grai-server/app/connections/tests/test_models.py
+++ grai-server/app/connections/tests/test_models.py
@@ -2,6 +2,7 @@ import datetime
 import uuid
 
 import pytest
+from django_celery_beat.models import PeriodicTask
 
 from connections.models import Connection, Connector, Run
 from workspaces.models import Organisation, Workspace
@@ -126,3 +127,40 @@ class TestConnection:
             connection.save()
 
         assert str(e_info.value) == "Schedule type not found"
+
+    @pytest.mark.django_db
+    def test_connection_updated_remove_schedule(self):
+        organisation = Organisation.objects.create(name="O1")
+        workspace = Workspace.objects.create(name="W1", organisation=organisation)
+        connector = Connector.objects.create(name="C1")
+        connection = Connection.objects.create(workspace=workspace, connector=connector, name="Connection 1")
+
+        connection.schedules = {
+            "type": "cron",
+            "cron": {
+                "minutes": "*",
+                "hours": "*",
+                "day_of_week": "*",
+                "day_of_month": "*",
+                "month_of_year": "*",
+            },
+        }
+
+        connection.save()
+
+        assert connection.id == uuid.UUID(str(connection.id))
+        assert str(connection) == "Connection 1"
+        assert connection.name == "Connection 1"
+        assert connection.task is not None
+
+        task = connection.task
+
+        connection.schedules = None
+        connection.save()
+
+        assert connection.task is None
+
+        with pytest.raises(PeriodicTask.DoesNotExist) as e_info:
+            PeriodicTask.objects.get(id=task.id)
+
+        assert str(e_info.value) == "PeriodicTask matching query does not exist."
