7358ad3ea200cc861c617fa90ceba1a57ad0e9db
Edward <edward@louths.org.uk>
2023-02-19 17:13:24 +0000

    Add tests

--- grai-server/app/connections/tests/test_models.py
+++ grai-server/app/connections/tests/test_models.py
@@ -126,3 +126,33 @@ class TestConnection:
             connection.save()
 
         assert str(e_info.value) == "Schedule type not found"
+
+    @pytest.mark.django_db
+    def test_connection_updated_remove_schedule(self):
+        organisation = Organisation.objects.create(name="O1")
+        workspace = Workspace.objects.create(name="W1", organisation=organisation)
+        connector = Connector.objects.create(name="C1")
+        connection = Connection.objects.create(workspace=workspace, connector=connector, name="Connection 1")
+
+        connection.schedules = {
+            "type": "cron",
+            "cron": {
+                "minutes": "*",
+                "hours": "*",
+                "day_of_week": "*",
+                "day_of_month": "*",
+                "month_of_year": "*",
+            },
+        }
+
+        connection.save()
+
+        assert connection.id == uuid.UUID(str(connection.id))
+        assert str(connection) == "Connection 1"
+        assert connection.name == "Connection 1"
+        assert connection.task is not None
+
+        connection.schedules = None
+        connection.save()
+
+        assert connection.task is None
