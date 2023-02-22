14d9664645303fe18a778805b017437185c1f90c
Edward Louth <edward@louths.com>
2023-02-22 13:34:28 +0000

    Fix for dbt namespaces (#219)

--- grai-server/app/connections/adapters/dbt.py
+++ grai-server/app/connections/adapters/dbt.py
@@ -10,14 +10,11 @@ class DbtAdapter(BaseAdapter):
 
         runFile = self.run.files.first()
 
-        namespace = "default"
-
-        # nodes, edges = get_nodes_and_edges(manifest_file=runFile.file.read(), namespace=namespace, version="v1")
         with runFile.file.open("r") as f:
             data = json.load(f)
 
         manifest = Manifest(**data)
-        dbt_graph = DBTGraph(manifest, namespace=namespace)
+        dbt_graph = DBTGraph(manifest, namespace=self.run.connection.namespace)
 
         nodes = adapt_to_client(dbt_graph.nodes, "v1")
         edges = adapt_to_client(dbt_graph.edges, "v1")
