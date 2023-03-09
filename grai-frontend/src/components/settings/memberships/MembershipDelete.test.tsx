28ef6bff515a1a1ed0c0e7add6e15b31c057d89a
Edward Louth <edward@louths.com>
2023-03-09 16:30:59 +0000

    Add test

--- grai-frontend/src/components/settings/memberships/MembershipDelete.test.tsx
+++ grai-frontend/src/components/settings/memberships/MembershipDelete.test.tsx
@@ -23,6 +23,26 @@ test("renders", async () => {
   )
 })
 
+test("renders no name", async () => {
+  const membership = {
+    id: "1",
+    user: {
+      username: "test@example.com",
+      first_name: null,
+      last_name: null,
+    },
+  }
+
+  const user = userEvent.setup()
+
+  render(<MembershipDelete membership={membership} onClose={() => {}} />)
+
+  await act(
+    async () =>
+      await user.click(screen.getByRole("menuitem", { name: /delete/i }))
+  )
+})
+
 // test("cancel", async () => {
 //   const user = userEvent.setup()
 
