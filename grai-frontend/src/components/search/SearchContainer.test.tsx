614862c25d2dd281873e420077d2844f381383c4
Edward Louth <edward@louths.com>
2023-03-15 12:41:30 +0000

    Add keyboard shortcuts

--- grai-frontend/src/components/search/SearchContainer.test.tsx
+++ grai-frontend/src/components/search/SearchContainer.test.tsx
@@ -1,25 +1,36 @@
 import React, { ReactNode } from "react"
 import userEvent from "@testing-library/user-event"
 import { GraphQLError } from "graphql"
-import { act, render, screen, waitFor } from "testing"
+import { act, fireEvent, render, screen, waitFor } from "testing"
 import SearchContainer, { GET_SEARCH_KEY } from "./SearchContainer"
+import {
+  useInstantSearch,
+  useHits,
+  useSearchBox,
+} from "react-instantsearch-hooks-web"
 
 const onClose = jest.fn()
 
 jest.mock("react-instantsearch-hooks-web", () => ({
   InstantSearch: ({ children }: { children: ReactNode }) => children,
-  useHits: () => ({ hits: [] }),
-  useSearchBox: () => ({
-    query: "",
-    refine: jest.fn(),
-    clear: jest.fn(),
-  }),
-  useInstantSearch: () => ({
-    error: undefined,
-  }),
+  useHits: jest.fn(),
+  useSearchBox: jest.fn(),
+  useInstantSearch: jest.fn(),
 }))
 
 test("renders", async () => {
+  ;(useInstantSearch as jest.Mock).mockReturnValue({
+    error: undefined,
+  })
+  ;(useHits as jest.Mock).mockReturnValue({
+    hits: [],
+  })
+  ;(useSearchBox as jest.Mock).mockReturnValue({
+    query: "",
+    refine: jest.fn(),
+    clear: jest.fn(),
+  })
+
   const user = userEvent.setup()
 
   render(<SearchContainer onClose={onClose} workspaceId="1" />, {
@@ -33,40 +44,62 @@ test("renders", async () => {
   await act(async () => await user.type(screen.getByRole("textbox"), "test"))
 })
 
-jest.mock("react-instantsearch-hooks-web", () => ({
-  InstantSearch: ({ children }: { children: ReactNode }) => children,
-  useHits: () => ({
+test("renders results", async () => {
+  ;(useInstantSearch as jest.Mock).mockReturnValue({
+    error: undefined,
+  })
+  ;(useHits as jest.Mock).mockReturnValue({
     hits: [
       {
         id: "1",
-        name: "test",
-        display_name: "test",
+        name: "hit1",
+        display_name: "hit1_display_name",
         search_type: "Table",
       },
+      {
+        id: "2",
+        name: "hit2",
+        display_name: "hit2_display_name",
+        search_type: "Column",
+      },
     ],
-  }),
-  useSearchBox: () => ({
+  })
+  ;(useSearchBox as jest.Mock).mockReturnValue({
     query: "test",
     refine: jest.fn(),
     clear: jest.fn(),
-  }),
-  useInstantSearch: () => ({
-    error: undefined,
-  }),
-}))
+  })
 
-test("renders results", async () => {
   const user = userEvent.setup()
 
   render(<SearchContainer onClose={onClose} workspaceId="1" />, {
-    withRouter: true,
+    routes: ["/:organisationName/:workspaceName/tables/:tableId"],
+  })
+
+  const textbox = screen.getByRole("textbox")
+
+  await waitFor(() => {
+    expect(textbox).toBeTruthy()
+  })
+
+  await act(async () => await user.type(textbox, "test"))
+
+  await waitFor(() => {
+    expect(screen.getByText("hit1")).toBeTruthy()
   })
 
   await waitFor(() => {
-    expect(screen.getByRole("textbox")).toBeTruthy()
+    expect(screen.getByText("hit2")).toBeTruthy()
   })
 
-  await act(async () => await user.type(screen.getByRole("textbox"), "test"))
+  await act(async () => await user.keyboard("{arrowdown}"))
+  await act(async () => await user.keyboard("{arrowdown}"))
+  await act(async () => await user.keyboard("{arrowup}"))
+  await act(async () => await user.keyboard("{enter}"))
+
+  await waitFor(() => {
+    expect(screen.getByText("New Page")).toBeInTheDocument()
+  })
 })
 
 test("error", async () => {
@@ -92,4 +125,30 @@ test("error", async () => {
   await waitFor(() => {
     expect(screen.getByText("Error!")).toBeInTheDocument()
   })
+})
+
+test("algolia error", async () => {
+  ;(useInstantSearch as jest.Mock).mockReturnValue({
+    error: Error('"validUntil" parameter expired (less than current date)'),
+  })
+  ;(useHits as jest.Mock).mockReturnValue({
+    hits: [],
+  })
+  ;(useSearchBox as jest.Mock).mockReturnValue({
+    query: "",
+    refine: jest.fn(),
+    clear: jest.fn(),
+  })
+
+  const user = userEvent.setup()
+
+  render(<SearchContainer onClose={onClose} workspaceId="1" />, {
+    withRouter: true,
+  })
+
+  await waitFor(() => {
+    expect(screen.getByRole("textbox")).toBeTruthy()
+  })
+
+  await act(async () => await user.type(screen.getByRole("textbox"), "test"))
 })
--- grai-frontend/src/components/search/SearchForm.tsx
+++ grai-frontend/src/components/search/SearchForm.tsx
@@ -1,5 +1,11 @@
-import React from "react"
+import React, { useState } from "react"
+import {
+  ArrowDownward,
+  ArrowUpward,
+  SubdirectoryArrowLeft,
+} from "@mui/icons-material"
 import { Divider, Box, List, Typography } from "@mui/material"
+import useWorkspace from "helpers/useWorkspace"
 import {
   useHits,
   useSearchBox,
@@ -14,14 +20,44 @@ type SearchFormProps = {
 }
 
 const SearchForm: React.FC<SearchFormProps> = ({ onClose, onError }) => {
+  const { workspaceNavigate } = useWorkspace()
+
   const { query } = useSearchBox()
   const { hits } = useHits<SearchHit>()
   const { error } = useInstantSearch()
 
+  const [selected, setSelected] = useState<string>()
+
   if (error) onError(error)
 
+  const handleKeyPress = (event: React.KeyboardEvent<HTMLDivElement>) => {
+    switch (event.code) {
+      case "ArrowDown":
+        const indexDown = hits.findIndex(hit => hit.id === selected)
+        const nextIndexDown = indexDown + 1
+        if (nextIndexDown < hits.length) {
+          setSelected(hits[nextIndexDown].id)
+        }
+        return
+      case "ArrowUp":
+        const indexUp = hits.findIndex(hit => hit.id === selected)
+        const nextIndexUp = indexUp - 1
+        if (nextIndexUp >= 0) {
+          setSelected(hits[nextIndexUp].id)
+        }
+        return
+      case "Enter":
+        const hit = hits.find(hit => hit.id === selected)
+        if (hit) {
+          workspaceNavigate(
+            hit.table_id ? `tables/${hit.table_id}` : `tables/${hit.id}`
+          )
+        }
+    }
+  }
+
   return (
-    <>
+    <Box onKeyDown={handleKeyPress}>
       <SearchTextbox onClose={onClose} />
       {query.length > 0 && (
         <>
@@ -29,7 +65,11 @@ const SearchForm: React.FC<SearchFormProps> = ({ onClose, onError }) => {
           <Box sx={{ height: 700, overflowY: "scroll" }}>
             <List>
               {hits.map(hit => (
-                <SearchHitRow key={hit.id} hit={hit} />
+                <SearchHitRow
+                  key={hit.id}
+                  hit={hit}
+                  selected={selected === hit.id}
+                />
               ))}
             </List>
             {hits.length === 0 && (
@@ -38,9 +78,35 @@ const SearchForm: React.FC<SearchFormProps> = ({ onClose, onError }) => {
               </Box>
             )}
           </Box>
+          <Divider />
+          <Box sx={{ p: 2, display: "flex" }}>
+            <Typography
+              sx={{
+                mr: 2,
+                display: "flex",
+                alignItems: "center",
+                flexWrap: "wrap",
+              }}
+            >
+              <SubdirectoryArrowLeft sx={{ mr: 1 }} />
+              to select
+            </Typography>
+            <Typography
+              sx={{
+                mr: 2,
+                display: "flex",
+                alignItems: "center",
+                flexWrap: "wrap",
+              }}
+            >
+              <ArrowUpward />
+              <ArrowDownward sx={{ mr: 1 }} /> to navigate
+            </Typography>
+            <Typography>esc to close</Typography>
+          </Box>
         </>
       )}
-    </>
+    </Box>
   )
 }
 
