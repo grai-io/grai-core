941d055bfa2e41ceae735bb29cc485fd9276991b
Ian Eaves <ian.k.eaves@gmail.com>
2023-03-21 11:32:38 -0500

    bug

--- grai-server/app/entrypoint.sh
+++ grai-server/app/entrypoint.sh
@@ -14,7 +14,6 @@ export GRAI_SERVER_VERSION=`poetry version -s`
 # Generate a default random key
 DEFAULT_KEY=`cat /dev/urandom | env LC_ALL=C tr -dc 'a-zA-Z0-9' | fold -w 50 | head -n 1`
 export SECRET_KEY=${SECRET_KEY:-$DEFAULT_KEY}
-python manage.py shell < "log.py" & exec "$@"
 
 #python manage.py flush --no-input
 python manage.py migrate
@@ -32,5 +31,5 @@ done <<< "$fixtures"
 # Every call to manage.py imports telemetry/apps which have a log event but we only want the first.
 # These should be set to false first if we don't want workers to log their deployment
 python manage.py shell < "init.py"
-
+python manage.py shell < "log.py"
 exec "$@"
