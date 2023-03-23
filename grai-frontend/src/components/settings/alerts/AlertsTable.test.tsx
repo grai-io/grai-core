6251972a77791fd726bea4c722e342164131a5a2
Lint Action <github-actions[bot]@users.noreply.github.com>
2023-03-23 10:44:18 +0000

    Fix code style issues with ESLint

--- grai-frontend/src/components/settings/alerts/AlertsTable.test.tsx
+++ grai-frontend/src/components/settings/alerts/AlertsTable.test.tsx
@@ -1,5 +1,5 @@
-import userEvent from "@testing-library/user-event"
 import React from "react"
+import userEvent from "@testing-library/user-event"
 import { act, render, screen } from "testing"
 import AlertsTable from "./AlertsTable"
 
