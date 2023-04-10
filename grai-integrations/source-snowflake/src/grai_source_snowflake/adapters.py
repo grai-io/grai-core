2feaa15f554b993350c9b53ef98d6b367c43cc3d
Ian Eaves <ian.k.eaves@gmail.com>
2023-04-10 10:20:58 -0500

    snowflake

--- grai-integrations/source-snowflake/src/grai_source_snowflake/adapters.py
+++ grai-integrations/source-snowflake/src/grai_source_snowflake/adapters.py
@@ -89,11 +89,11 @@ def build_grai_metadata_from_edge(current: Edge, version: Literal["v1"] = "v1") 
 
 
 @multimethod
-def build_snowflake_metadata(current: Any, desired: Any) -> None:
+def build_app_metadata(current: Any, desired: Any) -> None:
     raise NotImplementedError(f"No adapter between {type(current)} and {type(desired)} for value {current}")
 
 
-@build_snowflake_metadata.register
+@build_app_metadata.register
 def build_metadata_from_column(current: Column, version: Literal["v1"] = "v1") -> Dict:
     data = {
         "table_name": current.table,
@@ -103,7 +103,7 @@ def build_metadata_from_column(current: Column, version: Literal["v1"] = "v1") -
     return data
 
 
-@build_snowflake_metadata.register
+@build_app_metadata.register
 def build_metadata_from_table(current: Table, version: Literal["v1"] = "v1") -> Dict:
     data = {
         "schema": current.table_schema,
@@ -112,7 +112,7 @@ def build_metadata_from_table(current: Table, version: Literal["v1"] = "v1") -> 
     return data
 
 
-@build_snowflake_metadata.register
+@build_app_metadata.register
 def build_metadata_from_edge(current: Edge, version: Literal["v1"] = "v1") -> Dict:
     data = {
         "definition": current.definition,
@@ -127,9 +127,13 @@ def build_metadata_from_edge(current: Edge, version: Literal["v1"] = "v1") -> Di
 
 
 def build_metadata(obj, version):
+    integration_meta = build_app_metadata(obj, version)
+    base_metadata = build_grai_metadata(obj, version)
+    integration_meta["grai"] = base_metadata
+
     return {
-        base_config.metadata_id: build_grai_metadata(obj, version),
-        config.metadata_id: build_snowflake_metadata(obj, version),
+        base_config.metadata_id: base_metadata,
+        config.metadata_id: integration_meta,
     }
 
 
