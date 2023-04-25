a43d3706034a8e463c86fbd1eaf6bad5c3d48ef6
Edward Louth <edward@louths.com>
2023-04-25 16:54:39 +0100

    Add WelcomeCard and background colour

--- grai-frontend/src/pages/Home.tsx
+++ grai-frontend/src/pages/Home.tsx
@@ -1,18 +1,9 @@
 import React from "react"
 import { gql, useQuery } from "@apollo/client"
-import { Search } from "@mui/icons-material"
-import {
-  Box,
-  Button,
-  Card,
-  InputAdornment,
-  TextField,
-  Typography,
-} from "@mui/material"
-import { Link } from "react-router-dom"
+import { Box } from "@mui/material"
 import useWorkspace from "helpers/useWorkspace"
 import HomeCards from "components/home/HomeCards"
-import { GraiLogo, PersonAdd } from "components/icons"
+import WelcomeCard from "components/home/WelcomeCard"
 import PageLayout from "components/layout/PageLayout"
 import SearchDialog from "components/search/SearchDialog"
 import GraphError from "components/utils/GraphError"
@@ -60,69 +51,7 @@ const Home: React.FC = () => {
   return (
     <PageLayout>
       <Box sx={{ padding: "24px" }}>
-        <Card
-          elevation={0}
-          sx={{
-            borderRadius: "20px",
-            borderColor: "#8338EC",
-            borderWidth: "3px",
-            borderStyle: "solid",
-            padding: "16px",
-            height: "470px",
-            mb: "24px",
-          }}
-        >
-          <Box sx={{ display: "flex" }}>
-            <Box sx={{ flexGrow: 1 }} />
-            <Button
-              component={Link}
-              to="settings/memberships"
-              variant="outlined"
-              startIcon={<PersonAdd />}
-              sx={{
-                color: "#8338EC",
-                fontSize: "16px",
-                fontWeight: 600,
-                borderColor: "#8338EC24",
-                borderRadius: "8px",
-                py: 1,
-                px: 3,
-                boxShadow: "0 4px 6px #8338EC10",
-              }}
-            >
-              Invite User
-            </Button>
-          </Box>
-          <Box sx={{ textAlign: "center" }}>
-            <Box>
-              <GraiLogo />
-            </Box>
-            <Typography
-              variant="h4"
-              sx={{
-                color: "#1F2A37",
-                fontSize: 36,
-                fontWeight: 800,
-                mt: "20px",
-              }}
-            >
-              Welcome to Grai
-            </Typography>
-            <TextField
-              placeholder="Search data assets"
-              onClick={() => setSearch(true)}
-              disabled
-              sx={{ width: 620, mb: 15, mt: "40px" }}
-              InputProps={{
-                endAdornment: (
-                  <InputAdornment position="end">
-                    <Search />
-                  </InputAdornment>
-                ),
-              }}
-            />
-          </Box>
-        </Card>
+        <WelcomeCard search={search} setSearch={setSearch} />
         <HomeCards />
       </Box>
       <SearchDialog
