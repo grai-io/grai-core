bc8bf5d98c9271f88850bd1a9173b21a74ca35df
Edward Louth <edward@louths.com>
2023-04-25 19:34:50 +0100

    Fix for flakey test

--- grai-frontend/src/components/home/ReportsCard.test.tsx
+++ grai-frontend/src/components/home/ReportsCard.test.tsx
@@ -4,8 +4,56 @@ import { render, screen, waitFor } from "testing"
 import ReportsCard, { GET_REPORTS } from "./ReportsCard"
 
 test("renders", async () => {
+  const mocks = [
+    {
+      request: {
+        query: GET_REPORTS,
+        variables: {
+          organisationName: "",
+          workspaceName: "",
+        },
+      },
+      result: {
+        data: {
+          workspace: {
+            id: "1",
+            runs: {
+              data: [
+                {
+                  id: "1",
+                  status: "success",
+                  connection: {
+                    id: "1",
+                    name: "Connection 1",
+                    temp: false,
+                    connector: {
+                      id: "1",
+                      name: "Connector 1",
+                      icon: "icon",
+                    },
+                  },
+                  commit: null,
+                  created_at: "2021-01-01T00:00:00.000Z",
+                  started_at: "2021-01-01T00:00:00.000Z",
+                  finished_at: "2021-01-01T00:00:00.000Z",
+                  user: {
+                    id: "1",
+                    first_name: "first",
+                    last_name: "last",
+                  },
+                  metadata: {},
+                },
+              ],
+            },
+          },
+        },
+      },
+    },
+  ]
+
   render(<ReportsCard />, {
     withRouter: true,
+    mocks,
   })
 
   await waitFor(() => {
