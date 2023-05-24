7f8b9d11a370c62b916b291a338bd2c198ccf5ee
Edward Louth <edward@louths.com>
2023-05-24 10:44:18 +0100

    Add cache manager

---
+++ grai-server/app/lineage/cache.py
@@ -0,0 +1,109 @@
+import redis
+from redis import Redis
+
+from workspaces.models import Workspace
+
+
+class GraphCache:
+    manager: Redis
+    workspace: Workspace
+
+    def __init__(self, workspace: Workspace):
+        self.workspace = workspace
+
+        self.manager = redis.Redis(host="localhost", port=6379, db=0)
+
+    def clear_cache(self):
+        self.manager.delete(f"lineage:{str(self.workspace.id)}")
+
+    def cache_node(self, node):
+        node_type = node.metadata["grai"]["node_type"]
+
+        if node_type == "Table":
+            self.manager.graph(f"lineage:{str(self.workspace.id)}").query(
+                f"""
+                    MERGE (table:Table {{id: $id}})
+                    ON CREATE SET table.name = $name, table.namespace = $namespace, table.data_source = $data_source
+                    ON MATCH SET table.name = $name, table.namespace = $namespace, table.data_source = $data_source
+                """,
+                {
+                    "id": str(node.id),
+                    "name": node.name,
+                    "namespace": node.namespace,
+                    "data_source": node.data_source,
+                },
+            )
+
+        elif node_type == "Column":
+            self.manager.graph(f"lineage:{str(self.workspace.id)}").query(
+                f"""
+                    MERGE (column:Column {{id: $id}})
+                    ON CREATE SET column.name = $name
+                    ON MATCH SET column.name = $name
+                """,
+                {
+                    "id": str(node.id),
+                    "name": node.name,
+                },
+            )
+
+    def cache_edge(self, edge):
+        edge_type = edge.metadata["grai"]["edge_type"]
+
+        if edge_type == "TableToColumn":
+            self.manager.graph(f"lineage:{str(self.workspace.id)}").query(
+                f"""
+                    MATCH (table:Table), (column:Column)
+                    WHERE table.id = $source
+                    AND column.id = $destination
+                    MERGE (table)-[r:TABLE_TO_COLUMN]->(column)
+                """,
+                {
+                    "source": str(edge.source_id),
+                    "destination": str(edge.destination_id),
+                },
+            )
+        elif edge_type == "TableToTable":
+            self.manager.graph(f"lineage:{str(self.workspace.id)}").query(
+                f"""
+                    MATCH (source:Table), (destination:Table)
+                    WHERE source.id = $source
+                    AND destination.id = $destination
+                    MERGE (source)-[r:TABLE_TO_TABLE]->(destination)
+                """,
+                {
+                    "source": str(edge.source_id),
+                    "destination": str(edge.destination_id),
+                },
+            )
+        elif edge_type == "ColumnToColumn":
+            self.manager.graph(f"lineage:{str(self.workspace.id)}").query(
+                f"""
+                    MATCH (source:Column), (destination:Column)
+                    WHERE source.id = $source
+                    AND destination.id = $destination
+                    MERGE (source)-[r:COLUMN_TO_COLUMN]->(destination)
+                """,
+                {
+                    "source": str(edge.source_id),
+                    "destination": str(edge.destination_id),
+                },
+            )
+
+            source_table_edge = edge.source.destination_edges.filter(metadata__grai__edge_type="TableToColumn").first()
+            destination_table_edge = edge.destination.destination_edges.filter(
+                metadata__grai__edge_type="TableToColumn"
+            ).first()
+
+            self.manager.graph(f"lineage:{str(self.workspace.id)}").query(
+                f"""
+                  MATCH (source:Table), (destination:Table)
+                  WHERE source.id = $source
+                  AND destination.id = $destination
+                  MERGE (source)-[r:TABLE_TO_TABLE_COPY]->(destination)
+                """,
+                {
+                    "source": str(source_table_edge.source_id),
+                    "destination": str(destination_table_edge.source_id),
+                },
+            )
--- grai-server/app/lineage/management/commands/build_cache.py
+++ grai-server/app/lineage/management/commands/build_cache.py
@@ -1,9 +1,9 @@
-import redis
 from django.core.management.base import CommandError, CommandParser
 from django_multitenant.utils import set_current_tenant
 from django_tqdm import BaseCommand
 from query_chunk import chunk
 
+from lineage.cache import GraphCache
 from workspaces.models import Workspace
 
 
@@ -28,10 +28,11 @@ class Command(BaseCommand):
         except Workspace.DoesNotExist:
             raise CommandError('workspace "%s" does not exist' % workspace_id)
 
-        self.redis_connection = redis.Redis(host="localhost", port=6379, db=0)
+        self.cache = GraphCache(self.workspace)
 
         if options["delete"]:
-            self.clear_cache()
+            self.cache.clear_cache()
+            self.stdout.write(self.style.SUCCESS('Successfully cleared cache for workspace "%s"' % self.workspace.name))
 
         self.build_cache()
 
@@ -41,97 +42,11 @@ class Command(BaseCommand):
         node_tqdm = self.tqdm(total=self.workspace.nodes.count())
 
         for node in chunk(self.workspace.nodes.all(), 10000):
-            node_type = node.metadata["grai"]["node_type"]
-
-            if node_type == "Table":
-                self.redis_connection.graph(f"lineage:{str(self.workspace.id)}").query(
-                    f"""
-                        MERGE (table:Table {{id: $id}})
-                        ON CREATE SET table.name = $name, table.namespace = $namespace, table.data_source = $data_source
-                        ON MATCH SET table.name = $name, table.namespace = $namespace, table.data_source = $data_source
-                    """,
-                    {
-                        "id": str(node.id),
-                        "name": node.name,
-                        "namespace": node.namespace,
-                        "data_source": node.data_source,
-                    },
-                )
-
-            elif node_type == "Column":
-                self.redis_connection.graph(f"lineage:{str(self.workspace.id)}").query(
-                    f"""
-                        MERGE (column:Column {{id: $id}})
-                        ON CREATE SET column.name = $name
-                        ON MATCH SET column.name = $name
-                    """,
-                    {
-                        "id": str(node.id),
-                        "name": node.name,
-                    },
-                )
-
+            self.cache.cache_node(node)
             node_tqdm.update(1)
 
         edge_tqdm = self.tqdm(total=self.workspace.edges.count())
 
         for edge in chunk(self.workspace.edges.all(), 10000):
-            edge_type = edge.metadata["grai"]["edge_type"]
-
-            if edge_type == "TableToColumn":
-                self.redis_connection.graph(f"lineage:{str(self.workspace.id)}").query(
-                    f"""
-                        MATCH (table:Table), (column:Column)
-                        WHERE table.id = $source
-                        AND column.id = $destination
-                        MERGE (table)-[r:TABLE_TO_COLUMN]->(column)
-                    """,
-                    {
-                        "source": str(edge.source_id),
-                        "destination": str(edge.destination_id),
-                    },
-                )
-            elif edge_type == "TableToTable":
-                self.redis_connection.graph(f"lineage:{str(self.workspace.id)}").query(
-                    f"""
-                        MATCH (source:Table), (destination:Table)
-                        WHERE source.id = $source
-                        AND destination.id = $destination
-                        MERGE (source)-[r:TABLE_TO_TABLE]->(destination)
-                    """,
-                    {
-                        "source": str(edge.source_id),
-                        "destination": str(edge.destination_id),
-                    },
-                )
-            elif edge_type == "ColumnToColumn":
-                self.redis_connection.graph(f"lineage:{str(self.workspace.id)}").query(
-                    f"""
-                        MATCH (source:Column), (destination:Column)
-                        WHERE source.id = $source
-                        AND destination.id = $destination
-                        MERGE (source)-[r:COLUMN_TO_COLUMN]->(destination)
-                    """,
-                    {
-                        "source": str(edge.source_id),
-                        "destination": str(edge.destination_id),
-                    },
-                )
-
-                source_table_edge = edge.source.destination_edges.filter(
-                    metadata__grai__edge_type="TableToColumn"
-                ).first()
-                destination_table_edge = edge.destination.destination_edges.filter(
-                    metadata__grai__edge_type="TableToColumn"
-                ).first()
-
-                self.redis_connection.graph(f"lineage:{str(self.workspace.id)}").query(
-                    f"MATCH (source:Table), (destination:Table) WHERE source.id = '{str(source_table_edge.source_id)}' AND destination.id = '{str(destination_table_edge.source_id)}' MERGE (source)-[r:TABLE_TO_TABLE_COPY]->(destination)"
-                )
-
+            self.cache.cache_edge(edge)
             edge_tqdm.update(1)
-
-    def clear_cache(self):
-        self.redis_connection.delete(f"lineage:{str(self.workspace.id)}")
-
-        self.stdout.write(self.style.SUCCESS('Successfully cleared cache for workspace "%s"' % self.workspace.name))
