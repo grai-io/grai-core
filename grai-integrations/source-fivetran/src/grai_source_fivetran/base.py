1cd7710fec15c1f7879c19ea2e8d08ee46142308
Ian Eaves <ian.k.eaves@gmail.com>
2023-05-26 13:53:04 -0500

    alerting + tests

--- grai-integrations/source-fivetran/src/grai_source_fivetran/base.py
+++ grai-integrations/source-fivetran/src/grai_source_fivetran/base.py
@@ -9,11 +9,24 @@ from grai_source_fivetran.adapters import adapt_to_client
 from grai_source_fivetran.loader import FivetranConnector, NamespaceTypes
 
 
+def validate_nodes_and_edges(nodes: List[Node], edges: List[Edge]):
+    node_hashes = [hash(node) for node in nodes]
+    if (n_hashes := len(set(node_hashes))) == len(nodes):
+        message = (
+            f"The Fivetran connection generated {len(nodes) - n_hashes} duplicated nodes. "
+            f"This is likely because there are multiple Fivetran tables with the same name. "
+            f"You can disambiguate these tables by identifying them with different namespaces. Please "
+            f"see the documentation for more information. https://docs.grai.io/tooling/github-actions#fivetran"
+        )
+        raise ValueError(message)
+
+
 def get_nodes_and_edges(connector: FivetranConnector, version: Literal["v1"]) -> Tuple[List[Node], List[Edge]]:
     nodes, edges = connector.get_nodes_and_edges()
 
     nodes = adapt_to_client(nodes, version)
     edges = adapt_to_client(edges, version)
+    validate_nodes_and_edges(nodes, edges)
     return nodes, edges
 
 
