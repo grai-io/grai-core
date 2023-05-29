bcf8578f195c3f154993946fcdeb57f18bd9abc4
Ian Eaves <ian.k.eaves@gmail.com>
2023-05-29 12:38:05 -0500

    more-docs

--- actions/update_docs.py
+++ actions/update_docs.py
@@ -1,10 +1,18 @@
+"""
+Automatically updates the documentation for Grai's GitHub actions.
+
+WARNING:
+    * This script will overwrite any changes made to the documentation
+    * mdx is not fully compatible with md so if a page fails to render this is likely the cause.
+"""
 import json
 import os
+import re
 
 import yaml
 
 action_root = "./grai-actions"
-core_actions_docs_root = "../docs/pages/tooling/Github Actions"
+core_actions_docs_root = "../docs/pages/tooling/github-actions"
 
 documented_file = os.path.join(action_root, "docs/documented.yaml")
 with open(documented_file) as f:
@@ -24,14 +32,19 @@ def make_header(doc):
     return header_string
 
 
+def write_doc_file(str_obj, path):
+    with open(path, "w") as f:
+        output = re.sub("<!--.*?-->", "", str_obj)
+        f.write(output)
+
+
 for doc in documented:
     readme_file = os.path.join(action_root, doc["folder"], "README.md")
     readme = open(readme_file).read()
 
     output_file = os.path.join(core_actions_docs_root, f"{doc['folder']}.mdx")
     new_readme = f"{make_header(doc)}\n\n{readme}"
-    with open(output_file, "w") as f:
-        f.write(new_readme)
+    write_doc_file(new_readme, output_file)
 
 base_readme = os.path.join(action_root, "base_readme.md")
 base_readme = open(base_readme).read()
@@ -40,10 +53,9 @@ index_header_items = ["---", "title: Grai Actions", "description: Documentation 
 index_header = "\n".join(index_header_items)
 index_readme = f"{index_header}\n\n{base_readme}"
 
-index_file = os.path.join(core_actions_docs_root, "index.mdx")
-with open(index_file, "w") as f:
-    f.write(index_readme)
+index_file = f"{core_actions_docs_root}.mdx"
+write_doc_file(index_readme, index_file)
 
-_meta_json = {doc["folder"]: doc["name"] for doc in documented}
+_meta_json = {doc["folder"]: doc.get("name", doc["folder"]) for doc in documented}
 with open(os.path.join(core_actions_docs_root, "_meta.json"), "w") as f:
     json.dump(_meta_json, f)
