481b3652af0d4afa9d681f633ad6d5d5155fbefa
Ian Eaves <ian.k.eaves@gmail.com>
2023-06-02 12:42:24 -0500

    oops

--- grai-integrations/source-postgres/dev-database/init-db.sh
+++ grai-integrations/source-postgres/dev-database/init-db.sh
@@ -1,9 +1,9 @@
 SCRIPT_DIR=$(dirname -- "$0")/schemas
-PGUSER="${POSTGRES_USER-grai}"
-PGPASSWORD="${POSTGRES_PASSWORD-grai}"
-PG_DB="${POSTGRES_DB-grai}"
-HOST="{$HOST-localhost}"
-PORT="{PORT-'5433'}"
+PGUSER="${POSTGRES_USER:-grai}"
+PGPASSWORD="${POSTGRES_PASSWORD:-grai}"
+PG_DB="${POSTGRES_DB:-grai}"
+HOST="${HOST:-localhost}"
+PORT="${PORT:-5433}"
 
 
 for file in $(find $SCRIPT_DIR -type f -name '*.sql' | sort)
