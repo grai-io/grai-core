199e753b70c046b811472ac2e2de894457f30b77
Edward Louth <edward@louths.com>
2023-06-08 17:45:22 +0100

    Clear workspace (#430)
    * Add clearWorkspace mutation
    
    * Frontend
    
    * Fix code style issues with ESLint
    
    ---------
    
    Co-authored-by: Lint Action <github-actions[bot]@users.noreply.github.com>

--- grai-server/app/workspaces/tests/test_mutations.py
+++ grai-server/app/workspaces/tests/test_mutations.py
@@ -18,6 +18,7 @@ from api.tests.common import (
     test_workspace,
 )
 from workspaces.models import WorkspaceAPIKey
+from lineage.models import Edge, Node
 
 
 @pytest.mark.asyncio
@@ -117,6 +118,60 @@ async def test_update_workspace(test_context):
         "id": str(workspace.id),
         "name": "Test Workspace 2",
     }
+
+
+@pytest.mark.asyncio
+@pytest.mark.django_db
+async def test_clear_workspace(test_context):
+    context, organisation, workspace, user, membership = test_context
+
+    mutation = """
+        mutation ClearWorkspace($id: ID!) {
+            clearWorkspace(id: $id) {
+                id
+            }
+        }
+    """
+
+    result = await schema.execute(
+        mutation,
+        variable_values={
+            "id": str(workspace.id),
+        },
+        context_value=context,
+    )
+
+    assert result.errors is None
+    assert result.data["clearWorkspace"]["id"] == str(workspace.id)
+
+
+@pytest.mark.asyncio
+@pytest.mark.django_db
+async def test_clear_workspace_with_nodes(test_context):
+    context, organisation, workspace, user, membership = test_context
+
+    source = await Node.objects.acreate(workspace=workspace, name=str(uuid.uuid4()))
+    destination = await Node.objects.acreate(workspace=workspace, name=str(uuid.uuid4()))
+    await Edge.objects.acreate(workspace=workspace, source=source, destination=destination)
+
+    mutation = """
+        mutation ClearWorkspace($id: ID!) {
+            clearWorkspace(id: $id) {
+                id
+            }
+        }
+    """
+
+    result = await schema.execute(
+        mutation,
+        variable_values={
+            "id": str(workspace.id),
+        },
+        context_value=context,
+    )
+
+    assert result.errors is None
+    assert result.data["clearWorkspace"]["id"] == str(workspace.id)
 
 
 @pytest.mark.django_db
