1abb65291a3899811323d08ef8acdd62304801c5
Edward Louth <edward@louths.com>
2023-06-09 18:24:40 +0100

    Add React Helmet

--- grai-frontend/src/pages/auth/Login.tsx
+++ grai-frontend/src/pages/auth/Login.tsx
@@ -1,71 +1,84 @@
 import React from "react"
 import { Box, Card, CardContent, Container, Typography } from "@mui/material"
+import { Helmet } from "react-helmet-async"
 import LoginForm from "components/auth/login/LoginForm"
 import SignupLink from "components/auth/login/SignupLink"
 import GraiLogo from "components/icons/GraiLogo"
 
 const Login: React.FC = () => (
-  <Box
-    sx={{
-      width: "100%",
-      height: "100vh",
-      overflow: "hidden",
-      position: "relative",
-    }}
-  >
+  <>
+    <Helmet>
+      <title>Grai Cloud Login</title>
+      <meta
+        name="description"
+        content="Login to Grai Cloud. The easiest way to get started with data lineage. Create a free account today."
+      />
+    </Helmet>
     <Box
       sx={{
-        backgroundColor: "#8338EC20",
-        width: 810,
-        height: 568,
-        position: "absolute",
-        top: 490,
-        left: -318,
-        borderRadius: "50%",
-        filter: "blur(200px)",
-        zIndex: -1,
-      }}
-    />
-    <Box
-      sx={{
-        backgroundColor: "#3A86FF20",
-        width: 800,
-        height: 624,
-        position: "absolute",
-        top: 557,
-        right: -300,
-        borderRadius: "50%",
-        filter: "blur(200px)",
+        width: "100%",
+        height: "100vh",
         overflow: "hidden",
-        zIndex: -1,
+        position: "relative",
       }}
-    />
-
-    <Container maxWidth="sm" sx={{ pt: 10, maxWidth: { xs: 500 } }}>
-      <Box sx={{ textAlign: "center" }}>
-        <Box sx={{ mb: 5, ml: 1 }}>
-          <GraiLogo />
-        </Box>
-        <Typography variant="h6" sx={{ fontSize: 36, fontWeight: 800, mb: 5 }}>
-          Welcome Back!
-        </Typography>
-      </Box>
-      <Card
+    >
+      <Box
         sx={{
-          boxShadow: "0 10px 20px 0 rgb(0 0 0 / 0.04)",
-          borderRadius: "20px",
-          borderColor: "rgba(0, 0, 0, 0.06)",
-          borderWidth: 1,
-          borderStyle: "solid",
+          backgroundColor: "#8338EC20",
+          width: 810,
+          height: 568,
+          position: "absolute",
+          top: 490,
+          left: -318,
+          borderRadius: "50%",
+          filter: "blur(200px)",
+          zIndex: -1,
         }}
-      >
-        <CardContent sx={{ p: "20px" }}>
-          <LoginForm />
-        </CardContent>
-      </Card>
-      <SignupLink />
-    </Container>
-  </Box>
+      />
+      <Box
+        sx={{
+          backgroundColor: "#3A86FF20",
+          width: 800,
+          height: 624,
+          position: "absolute",
+          top: 557,
+          right: -300,
+          borderRadius: "50%",
+          filter: "blur(200px)",
+          overflow: "hidden",
+          zIndex: -1,
+        }}
+      />
+
+      <Container maxWidth="sm" sx={{ pt: 10, maxWidth: { xs: 500 } }}>
+        <Box sx={{ textAlign: "center" }}>
+          <Box sx={{ mb: 5, ml: 1 }}>
+            <GraiLogo />
+          </Box>
+          <Typography
+            variant="h6"
+            sx={{ fontSize: 36, fontWeight: 800, mb: 5 }}
+          >
+            Welcome Back!
+          </Typography>
+        </Box>
+        <Card
+          sx={{
+            boxShadow: "0 10px 20px 0 rgb(0 0 0 / 0.04)",
+            borderRadius: "20px",
+            borderColor: "rgba(0, 0, 0, 0.06)",
+            borderWidth: 1,
+            borderStyle: "solid",
+          }}
+        >
+          <CardContent sx={{ p: "20px" }}>
+            <LoginForm />
+          </CardContent>
+        </Card>
+        <SignupLink />
+      </Container>
+    </Box>
+  </>
 )
 
 export default Login
