0dd1e9377cd58e2bf85e820c3d1101e91c49fed0
Edward Louth <edward@louths.com>
2023-06-09 13:57:34 +0100

    Add social image to docs

--- docs/next-seo.config.js
+++ docs/next-seo.config.js
@@ -4,14 +4,24 @@ export default {
     locale: "en_US",
     url: "https://docs.grai.io/",
     siteName: "Grai Docs",
+    images: [
+      {
+        url: "https://uploads-ssl.webflow.com/6447319aa8cf8e6b22816e04/644e871d9dcb880ad28d3d1c_grai-site.png",
+        width: 800,
+        height: 600,
+        alt: "Grai Cloud",
+      },
+    ],
   },
   twitter: {
     handle: "@Grai_io",
     site: "@Grai_io",
     cardType: "summary_large_image",
   },
-  languageAlternates:[{
-    hrefLang: 'en-US',
-    href: process.env.WEB_URI,
-  }]
+  languageAlternates: [
+    {
+      hrefLang: "en-US",
+      href: process.env.WEB_URI,
+    },
+  ],
 };
