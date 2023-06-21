845d7ab65ebc44adc7ad486a4c1387132aa3d4d2
Lint Action <github-actions[bot]@users.noreply.github.com>
2023-06-21 11:21:25 +0000

    Fix code style issues with ESLint

--- grai-frontend/src/components/home/HomeCard.test.tsx
+++ grai-frontend/src/components/home/HomeCard.test.tsx
@@ -1,7 +1,7 @@
 import React from "react"
+import userEvent from "@testing-library/user-event"
 import { act, render, screen, waitFor } from "testing"
 import HomeCard from "./HomeCard"
-import userEvent from "@testing-library/user-event"
 
 test("renders", async () => {
   render(<HomeCard color="#FF00FF" text="test" />, {
