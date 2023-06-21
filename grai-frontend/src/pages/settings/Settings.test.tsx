67c1560c5fa11cfb8e343df29c02cbe0315b84a7
Edward Louth <edward@louths.com>
2023-06-21 12:02:05 +0100

    Test fix

--- grai-frontend/src/pages/settings/Settings.test.tsx
+++ grai-frontend/src/pages/settings/Settings.test.tsx
@@ -1,12 +1,8 @@
 import React from "react"
-import userEvent from "@testing-library/user-event"
-import { GraphQLError } from "graphql"
-import { act, render, screen, waitFor } from "testing"
-import Settings, { GET_WORKSPACE } from "./Settings"
+import { render, screen, waitFor } from "testing"
+import Settings from "./Settings"
 
-test("renders", async () => {
-  const user = userEvent.setup()
-
+test("redirects", async () => {
   render(<Settings />, {
     path: "/:organisationName/:workspaceName/settings",
     route: "/default/demo/settings",
@@ -14,62 +10,6 @@ test("renders", async () => {
   })
 
   await waitFor(() => {
-    expect(screen.getByText("Settings")).toBeInTheDocument()
-  })
-
-  await waitFor(() => {
-    expect(screen.getByTestId("AccountCircleIcon")).toBeInTheDocument()
-  })
-
-  await act(
-    async () => await user.click(screen.getByTestId("AccountCircleIcon"))
-  )
-})
-
-test("error", async () => {
-  const mocks = [
-    {
-      request: {
-        query: GET_WORKSPACE,
-        variables: {
-          organisationName: "",
-          workspaceName: "",
-        },
-      },
-      result: {
-        errors: [new GraphQLError("Error!")],
-      },
-    },
-  ]
-
-  render(<Settings />, { mocks, withRouter: true })
-
-  await waitFor(() => {
-    expect(screen.getByText("Error!")).toBeInTheDocument()
-  })
-})
-
-test("no workspace", async () => {
-  const mocks = [
-    {
-      request: {
-        query: GET_WORKSPACE,
-        variables: {
-          organisationName: "",
-          workspaceName: "",
-        },
-      },
-      result: {
-        data: {
-          workspace: null,
-        },
-      },
-    },
-  ]
-
-  render(<Settings />, { mocks, withRouter: true })
-
-  await waitFor(() => {
-    expect(screen.getByText("Page not found")).toBeInTheDocument()
+    expect(screen.getByText("New Page")).toBeInTheDocument()
   })
 })
