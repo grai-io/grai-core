ae42957c3a7e2d3de178fecf38dc5481eccdbe30
Edward Louth <edward@louths.com>
2023-06-29 14:10:09 +0100

    Update Redshift

--- grai-integrations/source-redshift/tests/conftest.py
+++ grai-integrations/source-redshift/tests/conftest.py
@@ -1,10 +1,11 @@
+import uuid
+
 import pytest
 from grai_schemas.v1.source import SourceSpec
 from grai_schemas.v1.workspace import WorkspaceSpec
 
-from grai_source_redshift.adapters import adapt_to_client
+from grai_source_redshift.base import RedshiftIntegration
 from grai_source_redshift.loader import RedshiftConnector
-from grai_source_redshift.models import Column, Edge, Table
 
 
 @pytest.fixture
@@ -32,10 +33,19 @@ def connection() -> RedshiftConnector:
     return connection
 
 
+class MockClient:
+    def __init__(self):
+        self.id = "v1"
+
+    def get(self, type, **kwargs):
+        return SourceSpec(id=uuid.uuid4(), **kwargs)
+
+
 @pytest.fixture
-def nodes_and_edges(tables, redshift_edges, mock_source):
-    nodes = adapt_to_client(tables, mock_source, "v1")
-    edges = adapt_to_client(redshift_edges, mock_source, "v1")
+def nodes_and_edges():
+    integration = RedshiftIntegration(MockClient(), "test", namespace="test")
+
+    nodes, edges = integration.get_nodes_and_edges()
     return nodes, edges
 
 
@@ -67,174 +77,3 @@ def edges(nodes_and_edges):
 
     """
     return nodes_and_edges[1]
-
-
-@pytest.fixture
-def column_params():
-    """ """
-    column_params = [
-        {
-            "name": "test",
-            "namespace": "test",
-            "data_type": "integer",
-            "is_nullable": True,
-        },
-        {
-            "column_name": "test",
-            "namespace": "test",
-            "data_type": "integer",
-            "is_nullable": True,
-        },
-        {
-            "column_name": "test",
-            "namespace": "test",
-            "data_type": "integer",
-            "is_nullable": True,
-            "default_value": 2,
-        },
-        {
-            "column_name": "test",
-            "namespace": "test",
-            "data_type": "integer",
-            "is_nullable": True,
-            "column_default": 2,
-        },
-    ]
-    shared = {"table": "test_table", "schema": "test_schema"}
-    for param in column_params:
-        param.update(shared)
-    return column_params
-
-
-@pytest.fixture
-def columns(column_params):
-    """
-
-    Args:
-        column_params:
-
-    Returns:
-
-    Raises:
-
-    """
-    return [Column(**params) for params in column_params]
-
-
-@pytest.fixture
-def table_params(column_params):
-    """
-
-    Args:
-        column_params:
-
-    Returns:
-
-    Raises:
-
-    """
-    table_params = [
-        {"name": "test", "namespace": "test", "schema": "test"},
-        {"table_name": "test", "namespace": "test", "table_schema": "test"},
-        {"table_name": "test", "namespace": "test", "schema": "test"},
-        {
-            "name": "test",
-            "schema": "test",
-            "namespace": "test",
-            "table_type": "BASE TABLE",
-        },
-        {"name": "test", "namespace": "test", "schema": "test", "columns": []},
-        {"name": "test", "namespace": "test", "schema": "test", "metadata": {}},
-        {
-            "name": "test",
-            "namespace": "test",
-            "schema": "test",
-            "table_type": "VIEW",
-            "metadata": {},
-            "columns": [],
-        },
-        {
-            "table_name": "test2",
-            "namespace": "test2",
-            "table_schema": "test2",
-            "name": "test2",
-        },
-    ]
-    for param in table_params:
-        param["table_dataset"] = "test"
-        param.setdefault("table_type", "BASE TABLE")
-    new_table = table_params[-1]
-    new_table["columns"] = column_params
-    table_params.append(new_table)
-    return table_params
-
-
-@pytest.fixture
-def tables(table_params):
-    """
-
-    Args:
-        table_params:
-
-    Returns:
-
-    Raises:
-
-    """
-    return [Table(**params) for params in table_params]
-
-
-@pytest.fixture
-def edge_params():
-    """ """
-
-    edge_params = [
-        {
-            "definition": "test",
-            "constraint_type": "PRIMARY KEY",
-            "destination": {
-                "table_name": "test2",
-                "namespace": "test2",
-                "table_schema": "test2",
-                "name": "test2",
-            },
-            "source": {
-                "table_name": "test2",
-                "namespace": "test2",
-                "table_schema": "test2",
-                "name": "test2",
-            },
-        },
-        {
-            "definition": "test",
-            "constraint_type": "FOREIGN KEY",
-            "destination": {
-                "table_name": "test2",
-                "namespace": "test2",
-                "table_schema": "test2",
-                "name": "test2",
-            },
-            "source": {
-                "table_name": "test2",
-                "namespace": "test2",
-                "table_schema": "test2",
-                "name": "test2",
-            },
-        },
-    ]
-    return edge_params
-
-
-@pytest.fixture
-def redshift_edges(edge_params):
-    """
-
-    Args:
-        edge_params:
-
-    Returns:
-
-    Raises:
-
-    """
-    return [Edge(**params) for params in edge_params]
