b92eb922fc1b47df3ec929b892a19933eba3644e
Edward Louth <edward@louths.com>
2023-06-29 19:14:18 +0100

    Small update

--- grai-client/src/grai_client/integrations/base.py
+++ grai-client/src/grai_client/integrations/base.py
@@ -1,5 +1,5 @@
 from abc import ABC, abstractmethod
-from typing import Generic, List, Tuple, TypeVar
+from typing import Generic, List, Optional, Tuple, TypeVar
 
 from grai_schemas.base import Event, SourcedEdge, SourcedNode
 from grai_schemas.v1.source import SourceSpec, SourceV1
@@ -17,41 +17,39 @@ class EventMixin(ABC):
         pass
 
     def update(self):
-        update(self.client, self.events())
         super().update()
+        update(self.client, self.events())
 
 
 T = TypeVar("T")
 
 
 class GraiIntegrationImplementationV1(Generic[T], ABC):
-    client: ClientV1
+    client: Optional[ClientV1]
     source: SourceV1
     params: T
+
+    def __init__(self, params: T, source: SourceV1, client: Optional[ClientV1] = None):
+        self.params = params
+        self.client = client
+        self.source = source
+
+        self.setup(params)
 
     @classmethod
     def from_client(cls, client: ClientV1, source_name: str, params: T):
         if client.id != "v1":
             raise NotImplementedError(f"No available implementation for client version {client.id}")
 
-        instance = cls()
+        source = client.get("Source", name=source_name)
 
-        instance.client = client
-        instance.params = params
-        instance.source = client.get("Source", name=source_name)
-        instance.setup(params)
-
-        return instance
+        return cls(params, source=source, client=client)
 
     @classmethod
     def from_source(cls, source: SourceSpec, params: T):
-        instance = cls()
+        source = SourceV1.from_spec(source)
 
-        instance.params = params
-        instance.source = SourceV1.from_spec(source)
-        instance.setup(params)
-
-        return instance
+        return cls(params, source=source)
 
     def setup(self):
         pass
