19944a49bb8deff929b327e9000ca2fbe025c643
Edward Louth <edward@louths.com>
2023-06-29 18:17:23 +0100

    Generic integration base

--- grai-client/src/grai_client/integrations/base.py
+++ grai-client/src/grai_client/integrations/base.py
@@ -1,8 +1,8 @@
 from abc import ABC, abstractmethod
-from typing import List, Tuple
+from typing import Generic, List, Tuple, TypeVar
 
 from grai_schemas.base import Event, SourcedEdge, SourcedNode
-from grai_schemas.v1.source import SourceV1
+from grai_schemas.v1.source import SourceSpec, SourceV1
 
 from grai_client.endpoints.v1.client import ClientV1
 from grai_client.update import update
@@ -21,16 +21,40 @@ class EventMixin(ABC):
         super().update()
 
 
-class GraiIntegrationImplementationV1(ABC):
+T = TypeVar("T")
+
+
+class GraiIntegrationImplementationV1(Generic[T], ABC):
     client: ClientV1
     source: SourceV1
+    params: T
 
-    def __init__(self, client: ClientV1, source_name: str):
+    @classmethod
+    def from_client(cls, client: ClientV1, source_name: str, params: T):
         if client.id != "v1":
             raise NotImplementedError(f"No available implementation for client version {client.id}")
 
-        self.client = client
-        self.source = client.get("Source", name=source_name)
+        instance = cls()
+
+        instance.client = client
+        instance.params = params
+        instance.source = client.get("Source", name=source_name)
+        instance.setup(params)
+
+        return instance
+
+    @classmethod
+    def from_source(cls, source: SourceSpec, params: T):
+        instance = cls()
+
+        instance.params = params
+        instance.source = SourceV1.from_spec(source)
+        instance.setup(params)
+
+        return instance
+
+    def setup(self):
+        pass
 
     @abstractmethod
     def nodes(self) -> List[SourcedNode]:
@@ -45,6 +69,9 @@ class GraiIntegrationImplementationV1(ABC):
         pass
 
     def update(self):
+        if not self.client:
+            raise Exception("Cannot update without a client")
+
         update(self.client, self.nodes())
         update(self.client, self.edges())
 
--- grai-integrations/source-bigquery/src/grai_source_bigquery/base.py
+++ grai-integrations/source-bigquery/src/grai_source_bigquery/base.py
@@ -1,45 +1,39 @@
-from typing import List, Literal, Optional, Tuple, Union
+from typing import List, Optional, Union
 
-from grai_client.endpoints.client import BaseClient
 from grai_client.integrations.base import (
     GraiIntegrationImplementationV1,
     SeparateNodesAndEdgesMixin,
 )
-from grai_client.update import update
-from grai_schemas.base import Edge, Node, Source
 
 from grai_source_bigquery.adapters import adapt_to_client
 from grai_source_bigquery.loader import BigqueryConnector, LoggingConnector
 
 
-class BigQueryIntegration(SeparateNodesAndEdgesMixin, GraiIntegrationImplementationV1):
-    def __init__(
-        self,
-        client: BaseClient,
-        source_name: str,
-        namespace: Optional[str] = None,
-        project: Optional[str] = None,
-        dataset: Optional[Union[str, List[str]]] = None,
-        credentials: Optional[str] = None,
-        log_parsing: Optional[bool] = False,
-        log_parsing_window: Optional[int] = 7,
-    ):
-        super().__init__(client, source_name)
+class BigQueryParams:
+    namespace: Optional[str] = None
+    project: Optional[str] = None
+    dataset: Optional[Union[str, List[str]]] = None
+    credentials: Optional[str] = None
+    log_parsing: Optional[bool] = False
+    log_parsing_window: Optional[int] = 7
 
+
+class BigQueryIntegration(SeparateNodesAndEdgesMixin, GraiIntegrationImplementationV1[BigQueryParams]):
+    def setup(self, params: BigQueryParams):
         self.connector = (
             BigqueryConnector(
-                project=project,
-                namespace=namespace,
-                dataset=dataset,
-                credentials=credentials,
+                project=params.project,
+                namespace=params.namespace,
+                dataset=params.dataset,
+                credentials=params.credentials,
             )
-            if not log_parsing
+            if not params.log_parsing
             else LoggingConnector(
-                project=project,
-                namespace=namespace,
-                dataset=dataset,
-                credentials=credentials,
-                window=log_parsing_window,
+                project=params.project,
+                namespace=params.namespace,
+                dataset=params.dataset,
+                credentials=params.credentials,
+                window=params.log_parsing_window,
             )
         )
 
