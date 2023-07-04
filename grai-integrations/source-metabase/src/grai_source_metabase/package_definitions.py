63af0636292b7bd608e1ffd981b9f0029f14058e
Elseagle <dowolebolu@gmail.com>
2023-07-04 04:13:40 +0100

    fix: change integration name

--- grai-integrations/source-metabase/src/grai_source_metabase/package_definitions.py
+++ grai-integrations/source-metabase/src/grai_source_metabase/package_definitions.py
@@ -2,7 +2,7 @@ from grai_schemas.generics import PackageConfig
 
 
 class Config(PackageConfig):
-    integration_name = "grai-schemas"
+    integration_name = "grai-source-metabase"
     metadata_id = "grai"
 
 
