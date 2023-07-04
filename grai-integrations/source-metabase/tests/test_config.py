d00cc9098a912933fa7add533714d185ab97993e
Elseagle <dowolebolu@gmail.com>
2023-07-04 03:19:39 +0100

    fix: change test endpoint

--- grai-integrations/source-metabase/tests/test_config.py
+++ grai-integrations/source-metabase/tests/test_config.py
@@ -7,14 +7,14 @@ def test_load_config_from_env_variables():
     env_vars = {
         "GRAI_METABASE_USERNAME": "test_user",
         "GRAI_METABASE_PASSWORD": "test_password",
-        "GRAI_METABASE_ENDPOINT": "https://data.inv.tech/api",
+        "GRAI_METABASE_ENDPOINT": "https://my.super.metabase/api",
     }
     os.environ.update(env_vars)
 
     config = MetabaseConfig()
     assert config.username.get_secret_value() == "test_user"
     assert config.password.get_secret_value() == "test_password"
-    assert config.endpoint == "https://data.inv.tech/api"
+    assert config.endpoint == "https://my.super.metabase/api"
     for k, v in env_vars.items():
         os.environ.pop(k)
 
@@ -23,22 +23,22 @@ def test_load_config_arguments():
     config = MetabaseConfig(
         username="test_user",
         password="test_password",
-        endpoint="https://data.inv.tech/api",
+        endpoint="https://my.super.metabase/api",
     )
     assert config.username.get_secret_value() == "test_user"
     assert config.password.get_secret_value() == "test_password"
-    assert config.endpoint == "https://data.inv.tech/api"
+    assert config.endpoint == "https://my.super.metabase/api"
 
 
 def test_config_has_default_endpoint():
     config = MetabaseConfig(username="test_user", password="test_password")
-    assert config.endpoint == "https://data.inv.tech/api"
+    assert config.endpoint == "https://my.super.metabase/api"
 
 
 def test_config_endpoint_handles_trailing_slash():
     config = MetabaseConfig(
         username="test_user",
         password="test_password",
-        endpoint="https://data.inv.tech/api/",
+        endpoint="https://my.super.metabase/api/",
     )
-    assert config.endpoint == "https://data.inv.tech/api"
+    assert config.endpoint == "https://my.super.metabase/api"
