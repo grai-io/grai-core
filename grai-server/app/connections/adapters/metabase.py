fe2444e4e2ef50907260c2d1e1ed4061ac6086c6
Edward Louth <edward@louths.com>
2023-07-11 11:41:01 +0100

    Metabase fixes

--- grai-server/app/connections/adapters/metabase.py
+++ grai-server/app/connections/adapters/metabase.py
@@ -2,16 +2,20 @@ from .base import BaseAdapter
 
 
 class MetabaseAdapter(BaseAdapter):
-    def get_nodes_and_edges(self):
+    def get_integration(self):
         from grai_source_metabase.base import MetabaseIntegration
 
         metadata = self.run.connection.metadata
         secrets = self.run.connection.secrets
 
         return MetabaseIntegration(
+            source={
+                "id": self.run.source.id,
+                "name": self.run.source.name,
+            },
             username=metadata.get("username"),
             password=secrets.get("password"),
-            namespaces=metadata.get("namespaces"),
+            namespace_map=metadata.get("namespaces"),
             metabase_namespace=self.run.connection.namespace,
             endpoint=metadata.get("endpoint"),
         )
