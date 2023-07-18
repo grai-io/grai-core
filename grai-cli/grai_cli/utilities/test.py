9bb8191a9d568d42459d80346cceba504d25acc0
Edward Louth <edward@louths.com>
2023-07-18 09:05:59 +0100

    Fix default workspace name

--- grai-cli/grai_cli/utilities/test.py
+++ grai-cli/grai_cli/utilities/test.py
@@ -8,7 +8,7 @@ def prep_test_auth():
     """ """
     from grai_cli import config
 
-    config.server = ServerSettingsV1(url="http://localhost:8000", workspace="default")
+    config.server = ServerSettingsV1(url="http://localhost:8000", workspace="default/default")
     config.auth = BasicAuthSettings(username="null@grai.io", password="super_secret")
 
 
