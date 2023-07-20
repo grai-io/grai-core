cbe1b5a0a07ed009ca3c280f3a630d3c87f354dd
Edward Louth <edward@louths.com>
2023-07-20 11:08:49 +0100

    Fix for source create cache (#506)
    * Fix for source create cache
    
    * Fix code style issues with ESLint
    
    ---------
    
    Co-authored-by: Lint Action <github-actions[bot]@users.noreply.github.com>

--- grai-frontend/src/components/sources/CreateSource.tsx
+++ grai-frontend/src/components/sources/CreateSource.tsx
@@ -8,6 +8,7 @@ import {
   CreateSource as CreateSourceType,
   CreateSourceVariables,
 } from "./__generated__/CreateSource"
+import { NewSource } from "./__generated__/NewSource"
 import SourceForm, { Values } from "./SourceForm"
 
 export const CREATE_SOURCE = gql`
@@ -35,7 +36,38 @@ const CreateSource: React.FC<CreateSourceProps> = ({ workspaceId }) => {
   const [createSource, { loading, error }] = useMutation<
     CreateSourceType,
     CreateSourceVariables
-  >(CREATE_SOURCE)
+  >(CREATE_SOURCE, {
+    update(cache, { data }) {
+      cache.modify({
+        id: cache.identify({
+          id: workspaceId,
+          __typename: "Workspace",
+        }),
+        fields: {
+          sources(existingSources = { data: [] }) {
+            if (!data?.createSource) return
+
+            const newSource = cache.writeFragment<NewSource>({
+              data: data.createSource,
+              fragment: gql`
+                fragment NewSource on Source {
+                  id
+                  name
+                }
+              `,
+            })
+            return {
+              data: [...existingSources.data, newSource],
+              meta: {
+                total: (existingSources.meta?.total ?? 0) + 1,
+                __typename: "SourcePagination",
+              },
+            }
+          },
+        },
+      })
+    },
+  })
 
   const handleSubmit = (values: Values) =>
     createSource({
@@ -47,7 +79,7 @@ const CreateSource: React.FC<CreateSourceProps> = ({ workspaceId }) => {
       .then(
         res =>
           res.data?.createSource.id &&
-          navigate(`${routePrefix}/sources/${res.data.createSource.id}`)
+          navigate(`${routePrefix}/sources/${res.data.createSource.id}`),
       )
       .then(() => enqueueSnackbar("Source created"))
       .catch(() => {})
