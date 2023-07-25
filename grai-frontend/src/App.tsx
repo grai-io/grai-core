169e191b5a8965153db6467fcde3668be15a1077
Edward <edward@louths.org.uk>
2023-07-25 10:46:54 -0500

    Fix page title

--- grai-frontend/src/App.tsx
+++ grai-frontend/src/App.tsx
@@ -8,7 +8,7 @@ import * as Sentry from "@sentry/react"
 import { BrowserTracing } from "@sentry/tracing"
 import { ConfirmProvider } from "material-ui-confirm"
 import { SnackbarKey, SnackbarProvider } from "notistack"
-import { HelmetProvider } from "react-helmet-async"
+import { Helmet, HelmetProvider } from "react-helmet-async"
 import {
   BrowserRouter,
   createRoutesFromChildren,
@@ -34,7 +34,7 @@ const App: React.FC = () => {
             useLocation,
             useNavigationType,
             createRoutesFromChildren,
-            matchRoutes
+            matchRoutes,
           ),
         }),
       ],
@@ -52,6 +52,13 @@ const App: React.FC = () => {
     <div>
       <CssBaseline />
       <HelmetProvider>
+        <Helmet>
+          <title>Grai Cloud</title>
+          <meta
+            name="description"
+            content="The easiest way to get started with data lineage. Create a free account today."
+          />
+        </Helmet>
         <ThemeProvider theme={theme}>
           <LocalizationProvider dateAdapter={AdapterLuxon}>
             <BackendProvider>
