ca0233c08d75d0d907f272550756ddd39b7326c7
Ian Eaves <ian.k.eaves@gmail.com>
2023-08-02 18:07:26 -0500

    some setup and docs

--- grai-integrations/source-looker/src/grai_source_looker/loader.py
+++ grai-integrations/source-looker/src/grai_source_looker/loader.py
@@ -32,5 +32,62 @@ T = TypeVar("T")
 P = ParamSpec("P")
 
 
-class Loader(api_settings.ApiSettings):
-    pass
+class LookerConfig(BaseSettings):
+    """ """
+
+    base_url: str
+    client_id: str
+    client_secret: SecretStr
+    verify_ssl: bool = True
+
+    @validator("base_url")
+    def validate_endpoint(cls, value):
+        """
+
+        Args:
+            value:
+
+        Returns:
+
+        Raises:
+
+        """
+        if (trailing_loc := value.find("/api")) > 0:
+            value = value[0:trailing_loc]
+        return value.rstrip("/")
+
+    class Config:
+        """ """
+
+        env_prefix = "grai_looker_"
+        env_file = ".env"
+
+
+# API authentication docs: https://cloud.google.com/looker/docs/api-auth
+# other docs https://cloud.google.com/looker/docs/reference/looker-api/latest/methods/Board/all_boards
+# sdk: https://github.com/looker-open-source/sdk-codegen/tree/main/python
+class LookerAPI:
+    """ """
+
+    def __init__(
+        self,
+        base_url: Optional[str] = None,
+        client_id: Optional[str] = None,
+        client_secret: Optional[str] = None,
+        verify_ssl: Optional[bool] = None,
+    ):
+        passthrough_kwargs = {
+            "base_url": base_url,
+            "client_id": client_id,
+            "client_secret": client_secret,
+            "verify_ssl": verify_ssl,
+        }
+        self.config = LookerConfig(**{k: v for k, v in passthrough_kwargs.items() if v is not None})
+
+        self.session = requests.Session()
+        self.session.auth = (
+            self.config.api_key.get_secret_value(),
+            self.config.api_secret.get_secret_value(),
+        )
+        self.session.headers.update({"Accept": "application/json"})
+        self.session.params.update({"limit": 10000 if limit is None else limit})
