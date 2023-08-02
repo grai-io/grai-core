7568f0d7e534287ab06f61e3eed89a4e6d91a670
Edward Louth <edward@louths.com>
2023-08-02 16:17:36 +0100

    Frontend tests

--- grai-frontend/src/components/connections/connectors/ConnectorCard.test.tsx
+++ grai-frontend/src/components/connections/connectors/ConnectorCard.test.tsx
@@ -1,6 +1,7 @@
 import React from "react"
-import { render } from "testing"
+import { act, render, screen, waitFor } from "testing"
 import ConnectorCard from "./ConnectorCard"
+import userEvent from "@testing-library/user-event"
 
 test("renders", async () => {
   render(
@@ -33,4 +34,54 @@ test("coming soon", async () => {
       withRouter: true,
     },
   )
+})
+
+test("click", async () => {
+  const user = userEvent.setup()
+
+  render(
+    <ConnectorCard
+      connector={{
+        id: "1",
+        name: "connector 1",
+        metadata: null,
+      }}
+      onSelect={() => {}}
+    />,
+    {
+      withRouter: true,
+    },
+  )
+
+  await act(async () => {
+    await user.click(screen.getByText("connector 1"))
+  })
+})
+
+test("to", async () => {
+  const user = userEvent.setup()
+
+  render(
+    <ConnectorCard
+      connector={{
+        id: "1",
+        name: "connector 1",
+        metadata: null,
+        to: "a",
+      }}
+      onSelect={() => {}}
+    />,
+    {
+      withRouter: true,
+      routes: ["/a"],
+    },
+  )
+
+  await act(async () => {
+    await user.click(screen.getByText("connector 1"))
+  })
+
+  await waitFor(() => {
+    expect(screen.getByText("New Page")).toBeTruthy()
+  })
 })
--- grai-frontend/src/components/wizards/WizardSubtitle.test.tsx
+++ grai-frontend/src/components/wizards/WizardSubtitle.test.tsx
@@ -1,6 +1,7 @@
 import React from "react"
 import { render, screen } from "testing"
 import WizardSubtitle from "./WizardSubtitle"
+import { Search } from "@mui/icons-material"
 
 test("renders", async () => {
   render(<WizardSubtitle />)
@@ -14,6 +15,12 @@ test("renders title", async () => {
 
 test("renders title icon", async () => {
   render(<WizardSubtitle title="Test Title" icon="/icons/file-icon.png" />)
+
+  expect(screen.getByText("Test Title")).toBeInTheDocument()
+})
+
+test("renders title react icon", async () => {
+  render(<WizardSubtitle title="Test Title" icon={<Search />} />)
 
   expect(screen.getByText("Test Title")).toBeInTheDocument()
 })
