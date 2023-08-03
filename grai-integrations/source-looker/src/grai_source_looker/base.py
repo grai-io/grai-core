663455fef235a86727dc7ae2dfd398d13089c63a
Edward Louth <edward@louths.com>
2023-08-03 18:18:00 +0100

    Remove validate method

--- grai-integrations/source-looker/src/grai_source_looker/base.py
+++ grai-integrations/source-looker/src/grai_source_looker/base.py
@@ -36,35 +36,10 @@ class LookerIntegration(CombinedNodesAndEdgesMixin, GraiIntegrationImplementatio
         self.connector.get_user()
         return True
 
-    def validate_nodes_and_edges(self, nodes: List[SourcedNode], edges: List[SourcedEdge]):
-        """
-
-        Args:
-            nodes (List[Node]):
-            edges (List[Edge]):
-
-        Returns:
-
-        Raises:
-
-        """
-        node_hashes = {hash(node) for node in nodes}
-        if (n_hashes := len(node_hashes)) != len(nodes):
-            message = (
-                f"The Fivetran connection generated {len(nodes) - n_hashes} duplicated nodes. "
-                f"This is likely because there are multiple Fivetran tables with the same name. "
-                f"You can disambiguate these tables by identifying them with different namespaces. Please "
-                f"see the documentation for more information. https://docs.grai.io/integrations/fivetran"
-            )
-            raise ValueError(message)
-
     def get_nodes_and_edges(self) -> Tuple[List[SourcedNode], List[SourcedEdge]]:
         nodes, edges = self.connector.get_nodes_and_edges()
 
-        print(nodes)
-        print(edges)
-
         nodes = adapt_to_client(nodes, self.source, self.version)
         edges = adapt_to_client(edges, self.source, self.version)
-        # self.validate_nodes_and_edges(nodes, edges)
+
         return nodes, edges
