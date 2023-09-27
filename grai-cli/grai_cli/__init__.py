7418a09e0fc9bae32578e5da6a385f84e09b55ea
Ian Eaves <ian.k.eaves@gmail.com>
2023-09-27 11:50:25 -0500

    release cli

--- grai-cli/grai_cli/__init__.py
+++ grai-cli/grai_cli/__init__.py
@@ -1,8 +1,7 @@
-from grai_cli import api, settings, utilities
-from grai_cli.settings.config import config
+from grai_cli import settings, utilities
 
 if settings.cache.cache.first_install:
     settings.cache.cache.set("first_install", False)
     utilities.telemetry.Telemetry.capture("First install")
 
-__version__ = "0.2.1"
+__version__ = "0.2.2"
