cc5089a0a8c6e822c904e22229114d32ec8f6d1a
Ian Eaves <ian.k.eaves@gmail.com>
2023-09-28 13:15:31 -0500

    tests

--- grai-client/tests/test_project_config.py
+++ grai-client/tests/test_project_config.py
@@ -1,5 +1,6 @@
 import os
-import tomllib
+
+import toml
 
 from grai_client import __version__
 
@@ -7,7 +8,7 @@ from grai_client import __version__
 def test_correct_version():
     toml_file = os.path.join(os.path.dirname(__file__), "../pyproject.toml")
     with open(toml_file, "rb") as f:
-        pyproject = tomllib.load(f)
+        pyproject = toml.load(f)
 
     version = pyproject["tool"]["poetry"]["version"]
     assert version == __version__
