cda7fcae3751dffa9b55909d1b9370d931390771
Ian Eaves <ian.k.eaves@gmail.com>
2023-09-28 13:45:43 -0500

    api differences between toml and tomllib

--- grai-client/tests/test_project_config.py
+++ grai-client/tests/test_project_config.py
@@ -7,8 +7,6 @@ from grai_client import __version__
 
 def test_correct_version():
     toml_file = os.path.join(os.path.dirname(__file__), "../pyproject.toml")
-    with open(toml_file, "rb") as f:
-        pyproject = toml.load(f)
-
+    pyproject = toml.load(toml_file)
     version = pyproject["tool"]["poetry"]["version"]
     assert version == __version__
