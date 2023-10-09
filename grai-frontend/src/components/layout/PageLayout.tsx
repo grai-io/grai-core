a5dad9ab041230e92982a0d212b3483c146a2fb9
Edward Louth <edward@louths.com>
2023-10-09 14:30:41 +0100

    Getting started banner

--- grai-frontend/src/components/layout/PageLayout.tsx
+++ grai-frontend/src/components/layout/PageLayout.tsx
@@ -3,6 +3,40 @@ import { Box } from "@mui/material"
 import ErrorBoundary from "components/utils/ErrorBoundary"
 import AppDrawer from "./AppDrawer"
 import Loading from "./Loading"
+import GettingStarted from "./GettingStarted"
+import { gql, useQuery } from "@apollo/client"
+import useWorkspace from "helpers/useWorkspace"
+import {
+  GetWorkspacePageLayout,
+  GetWorkspacePageLayoutVariables,
+} from "./__generated__/GetWorkspacePageLayout"
+
+export const GET_WORKSPACE = gql`
+  query GetWorkspacePageLayout(
+    $organisationName: String!
+    $workspaceName: String!
+  ) {
+    workspace(organisationName: $organisationName, name: $workspaceName) {
+      id
+      name
+      runs(filters: { action: TESTS }) {
+        meta {
+          filtered
+        }
+      }
+      nodes(filters: { node_type: { equals: "Table" } }) {
+        meta {
+          filtered
+        }
+      }
+      connections {
+        meta {
+          total
+        }
+      }
+    }
+  }
+`
 
 type PageLayoutProps = {
   children?: ReactNode
@@ -14,23 +48,44 @@ const PageLayout: React.FC<PageLayoutProps> = ({
   children,
   loading,
   padding,
-}) => (
-  <Box sx={{ display: "flex" }}>
-    <AppDrawer />
-    {loading && <Loading />}
-    <ErrorBoundary>
-      <Box
-        sx={{
-          padding: padding ? 3 : undefined,
-          flexGrow: 1,
-          backgroundColor: "#F8F8F8",
-          minHeight: "100vh",
-        }}
-      >
-        {children}
+}) => {
+  const { organisationName, workspaceName } = useWorkspace()
+
+  const { data } = useQuery<
+    GetWorkspacePageLayout,
+    GetWorkspacePageLayoutVariables
+  >(GET_WORKSPACE, {
+    variables: {
+      organisationName,
+      workspaceName,
+    },
+  })
+
+  const hasConnections = (data?.workspace.connections.meta.total ?? 0) > 0
+
+  return (
+    <>
+      {data && !hasConnections && <GettingStarted />}
+      <Box sx={{ display: "flex" }}>
+        <AppDrawer />
+        <Box sx={{ width: "100%" }}>
+          {loading && <Loading />}
+          <ErrorBoundary>
+            <Box
+              sx={{
+                padding: padding ? 3 : undefined,
+                flexGrow: 1,
+                backgroundColor: "#F8F8F8",
+                minHeight: "calc(100vh - 64px)",
+              }}
+            >
+              {children}
+            </Box>
+          </ErrorBoundary>
+        </Box>
       </Box>
-    </ErrorBoundary>
-  </Box>
-)
+    </>
+  )
+}
 
 export default PageLayout
