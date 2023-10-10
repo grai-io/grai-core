cde5440d83fc7cd44b34092761dc7402beb66e62
Ian Eaves <ian.k.eaves@gmail.com>
2023-10-10 12:59:32 -0500

    dependabot workarounds

--- grai-server/app/the_guide/settings/dev.py
+++ grai-server/app/the_guide/settings/dev.py
@@ -5,7 +5,7 @@ from ..settings.base import *
 # SECURITY WARNING: keep the secret key used in production secret!
 
 SECRET_KEY = "This-Is-A-Test_key"
-USER_ID = hashlib.md5(SECRET_KEY.encode()).hexdigest()
+USER_ID = hashlib.sha224(SECRET_KEY.encode()).hexdigest()
 
 DISABLE_TELEMETRY = config("DISABLE_TELEMETRY", default=True, cast=cast_string_to_bool(True))
 
