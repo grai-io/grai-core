0f90b79130b63b89565bb7f28705ff2db4cf316b
Edward Louth <edward@louths.com>
2023-10-20 09:27:45 +0100

    Get tests working

--- grai-integrations/source-openlineage/tests/test_base.py
+++ grai-integrations/source-openlineage/tests/test_base.py
@@ -1,14 +1,16 @@
 import os
 
 from grai_source_openlineage.processor import OpenLineageProcessor
-from grai_source_openlineage.specs import v1_0_0
 
 
 class TestOpenLineageProcessor:
     @staticmethod
     def test_processing_event(openlineage_test_event, mock_source):
         processor = OpenLineageProcessor(
-            lineage=openlineage_test_event, source=mock_source.spec, namespace="default", namespaces={}
+            lineage=openlineage_test_event,
+            source=mock_source.spec,
+            namespace="default",
+            namespaces={},
         )
 
 
