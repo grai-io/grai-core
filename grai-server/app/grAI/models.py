df6ab3452c23a6dd5d6adb36afe6995efce98176
Ian Eaves <ian.k.eaves@gmail.com>
2023-10-23 09:53:06 -0500

    refactored models

--- grai-server/app/grAI/models.py
+++ grai-server/app/grAI/models.py
@@ -6,37 +6,29 @@ from enum import Enum
 class UserChat(models.Model):
     """ """
 
+    chat_id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    membership = models.ForeignKey("workspaces.Membership", related_name="membership", on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        indexes = [
+            models.Index(fields=["chat_id"]),
+        ]
+
+
+class Message(models.Model):
     USER = "user"
     AGENT = "agent"
     SYSTEM = "system"
     ROLES = [(USER, "user"), (AGENT, "agent"), (SYSTEM, "system")]
 
-    id = models.AutoField(primary_key=True)
-    membership = models.ForeignKey("workspaces.Membership", related_name="membership", on_delete=models.CASCADE)
-    chat_id = models.UUIDField(default=uuid.uuid4, editable=False)
-    message_number = models.IntegerField(default=0)
-    role = models.CharField(max_length=255, choices=ROLES, default=USER)
+    chat = models.ForeignKey(UserChat, on_delete=models.CASCADE, related_name="messages")
     message = models.TextField()
+    visible = models.BooleanField()
+    role = models.CharField(max_length=255, choices=ROLES, default=USER)
     created_at = models.DateTimeField(auto_now_add=True)
 
-    def save(self, *args, **kwargs):
-        if self.chat_id is None:
-            self.message_number = None
-        elif self.message_number is None:
-            max_message_number = UserChat.objects.filter(membership=self.membership, chat_id=self.chat_id).aggregate(
-                models.Max("message_number")
-            )["message_number__max"]
-            self.message_number = max_message_number + 1
-        super().save(*args, **kwargs)
-
     class Meta:
-        constraints = [
-            models.UniqueConstraint(
-                fields=["chat_id", "message_number"],
-                name="Message numbers are unique for each chat",
-            ),
-        ]
         indexes = [
-            models.Index(fields=["chat_id"]),
-            models.Index(fields=["membership", "created_at"]),
+            models.Index(fields=["created_at"]),
         ]
