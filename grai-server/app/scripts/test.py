57c3cca30df499d663b9bf5e41ca19d627b1291e
Ian Eaves <ian.k.eaves@gmail.com>
2023-11-02 22:24:45 -0500

    Bearer handling

--- grai-server/app/scripts/test.py
+++
@@ -1,9 +0,0 @@
-import os
-
-import django
-
-os.environ["DJANGO_SETTINGS_MODULE"] = "the_guide.settings.dev"
-django.setup()
-import lineage.serializers as s
-
-print(repr(s.EdgeSerializer()))
--- grai-server/app/workspaces/permissions.py
+++ grai-server/app/workspaces/permissions.py
@@ -1,6 +1,16 @@
-from rest_framework_api_key.permissions import BaseHasAPIKey
-
+from rest_framework_api_key.permissions import BaseHasAPIKey, KeyParser, APIKey
+from django.http import HttpRequest
+import re
 from .models import WorkspaceAPIKey
+
+
+class BearerKeyParser(KeyParser):
+    keyword = "Bearer"
+
+
+class BearerApiKey(BaseHasAPIKey):
+    model = APIKey
+    key_parser = BearerKeyParser()
 
 
 class HasWorkspaceAPIKey(BaseHasAPIKey):
