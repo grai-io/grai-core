d8dd77bdaff9440a7b2f6363cadbdc46c30dbd7b
Lint Action <github-actions[bot]@users.noreply.github.com>
2023-11-11 18:31:15 +0000

    Fix code style issues with ESLint

--- grai-frontend/src/components/chat/ChatMessage.tsx
+++ grai-frontend/src/components/chat/ChatMessage.tsx
@@ -1,11 +1,11 @@
 import React from "react"
 import { Avatar, Box, Grid, Typography } from "@mui/material"
-import theme from "theme"
-import { GraiIconSmall } from "components/icons"
 import Markdown from "react-markdown"
-import remarkGfm from "remark-gfm";
 import { Prism as SyntaxHighlighter } from 'react-syntax-highlighter'
 import { dark } from 'react-syntax-highlighter/dist/esm/styles/prism'
+import remarkGfm from "remark-gfm";
+import theme from "theme"
+import { GraiIconSmall } from "components/icons"
 
 
 export type GroupedChats = {
