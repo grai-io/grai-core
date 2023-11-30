242df0e85495e45356a003a8240f0b69e27b7ca2
Edward Louth <edward@louths.com>
2023-11-30 10:28:25 +0000

    Highlight app drawer item (#827)

--- grai-frontend/src/components/layout/AppDrawerItem.test.tsx
+++ grai-frontend/src/components/layout/AppDrawerItem.test.tsx
@@ -1,46 +1,45 @@
 import { render, screen } from "testing"
 import AppDrawerItem from "./AppDrawerItem"
 
+const page = {
+  title: "test",
+  path: "/",
+  icon: <>Test Icon</>,
+}
+
 test("renders", async () => {
-  render(
-    <AppDrawerItem
-      title="test"
-      path="/"
-      icon={<>Test Icon</>}
-      expanded={false}
-    />,
-    { withRouter: true },
-  )
+  render(<AppDrawerItem page={page} expanded={false} />, { withRouter: true })
 
   expect(screen.getByText("Test Icon")).toBeInTheDocument()
   expect(screen.queryByText("test")).not.toBeInTheDocument()
 })
 
 test("renders expanded", async () => {
-  render(
-    <AppDrawerItem title="test" path="/" icon={<>Test Icon</>} expanded />,
-    {
-      withRouter: true,
-    },
-  )
+  render(<AppDrawerItem page={page} expanded />, {
+    withRouter: true,
+  })
 
   expect(screen.getByText("Test Icon")).toBeInTheDocument()
   expect(screen.getByText("test")).toBeInTheDocument()
 })
 
 test("alert", async () => {
-  render(
-    <AppDrawerItem
-      title="test"
-      path="/"
-      icon={<>Test Icon</>}
-      expanded={false}
-      alert
-    />,
-    { withRouter: true },
-  )
+  render(<AppDrawerItem page={{ ...page, alert: true }} expanded={false} />, {
+    withRouter: true,
+  })
 
   expect(screen.getByText("Test Icon")).toBeInTheDocument()
   expect(screen.queryByText("test")).not.toBeInTheDocument()
   expect(screen.getByTestId("app-drawer-item-alert")).toBeInTheDocument()
+})
+
+test("renders selected", async () => {
+  render(<AppDrawerItem page={{ ...page, path: "test" }} expanded />, {
+    withRouter: true,
+    route: "/default/demo/test",
+    path: "/:organisationName/:workspaceName/test",
+  })
+
+  expect(screen.getByText("Test Icon")).toBeInTheDocument()
+  expect(screen.getByText("test")).toBeInTheDocument()
 })
