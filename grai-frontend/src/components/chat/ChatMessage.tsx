69c6930ef2e1212085ecf2605e9c87166051d596
Ian Eaves <ian.k.eaves@gmail.com>
2023-12-18 16:41:43 -0600

    updated chat ux

--- grai-frontend/src/components/chat/ChatMessage.tsx
+++ grai-frontend/src/components/chat/ChatMessage.tsx
@@ -3,6 +3,7 @@ import { Avatar, Box } from "@mui/material"
 import theme from "theme"
 import { GraiIconSmall } from "components/icons"
 import Markdown from "components/utils/Markdown"
+import Typography from '@mui/material/Typography';
 
 export type GroupedChats = {
   sender: boolean
@@ -13,117 +14,122 @@ const radius = theme.spacing(2.5)
 const rightBgColor = "rgba(131, 56, 236)"
 
 const classes = {
-  left: {
+  agent: {
     borderTopRightRadius: radius,
     borderBottomRightRadius: radius,
-    backgroundColor: theme.palette.grey[100],
+    // backgroundColor: theme.palette.grey[100],
   },
-  right: {
-    borderTopLeftRadius: radius,
-    borderBottomLeftRadius: radius,
-    backgroundColor: rightBgColor,
-    color: theme.palette.common.white,
-  },
-  leftFirst: {
-    borderTopLeftRadius: radius,
-  },
-  leftLast: {
-    borderBottomLeftRadius: radius,
-  },
-  rightFirst: {
+  user: {
     borderTopRightRadius: radius,
-  },
-  rightLast: {
     borderBottomRightRadius: radius,
+    // backgroundColor: rightBgColor,
+    // color: theme.palette.common.white,
   },
 }
+
+const userLabel = {
+  agent: "Grai",
+  user: "You",
+}
+
+type AvatarComponentProps = {
+  sender: string;
+};
+
+const AvatarComponent: React.FC<AvatarComponentProps> = ({ sender }) => {
+  let avatar: JSX.Element;
+
+  if (sender == "agent") {
+    avatar = (
+      <Avatar
+        sx={{
+          backgroundColor: "white",
+          borderColor: rightBgColor,
+          borderWidth: 2,
+          borderStyle: "solid",
+        }}
+      >
+        <GraiIconSmall />
+      </Avatar>
+    );
+  } else {
+    avatar = (<Avatar />);
+  }
+
+  return (
+    <Box>
+      {avatar}
+    </Box>
+  );
+};
+
+type MessageComponentProps = {
+  sender: string;
+  message: string;
+};
+
+const MessageComponent: React.FC<MessageComponentProps> = ({ sender, message }) => {
+  let messageResponse: JSX.Element;
+
+  return (
+    <Box>
+      <Box
+        sx={{
+          display: "flex",
+          flexDirection: "row",
+        }}
+      >
+        <Box>
+          <AvatarComponent sender={sender}/>
+        </Box>
+        <Box sx = {{px: 2}}>
+          <Typography
+            sx={{
+              fontWeight: 'bold',
+            }}
+          >
+            {userLabel[sender]}
+          </Typography>
+          <Box
+            sx={{
+              mb: 0.5,
+              display: "inline-block",
+              wordBreak: "break-word",
+              fontFamily:
+                // eslint-disable-next-line max-len
+                '-apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, Helvetica, Arial, sans-serif, "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol"',
+              fontSize: "14px",
+              borderRadius: 1,
+              ...classes[sender],
+            }}
+          >
+            <Markdown message={message} />
+          </Box>
+        </Box>
+      </Box>
+    </Box>
+  );
+
+
+};
 
 type ChatMessageProps = {
   groupedChat: GroupedChats
 }
 
+
 const ChatMessage: React.FC<ChatMessageProps> = ({ groupedChat }) => {
-  const side = groupedChat.sender ? "right" : "left"
-
+  const sender = groupedChat.sender ? "user" : "agent"
   const messages = groupedChat.messages
-
-  const attachClass = (index: number) => {
-    if (index === 0) {
-      return classes[`${side}First`]
-    }
-    if (index === messages.length - 1) {
-      return classes[`${side}Last`]
-    }
-    return ""
-  }
 
   return (
     <Box sx={{ display: "flex", mb: 2 }}>
-      <Box
-        sx={{
-          display: "flex",
-          justifyContent: "flex-end",
-          flexDirection: "column",
-          width: "48px",
-          mr: 1,
-        }}
-      >
-        <Box>
-          {side === "left" && (
-            <Avatar
-              sx={{
-                backgroundColor: "white",
-                borderColor: rightBgColor,
-                borderWidth: 2,
-                borderStyle: "solid",
-              }}
-            >
-              <GraiIconSmall />
-            </Avatar>
-          )}
-        </Box>
-      </Box>
-
       <Box sx={{ width: "100%" }}>
         {messages.map((msg, i) => (
-          <Box
-            key={i}
-            sx={{
-              textAlign: side === "right" ? "right" : "left",
-            }}
-          >
-            <Box
-              sx={{
-                px: 2,
-                py: 1,
-                mb: 0.5,
-                display: "inline-block",
-                wordBreak: "break-word",
-                fontFamily:
-                  // eslint-disable-next-line max-len
-                  '-apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, Helvetica, Arial, sans-serif, "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol"',
-                fontSize: "14px",
-                borderRadius: 1,
-                ...attachClass(i),
-                ...classes[side],
-              }}
-            >
-              <Markdown message={msg} />
-            </Box>
+          <Box key={i}>
+            <MessageComponent sender={sender} message={msg}/>
           </Box>
         ))}
-      </Box>
-
-      <Box
-        sx={{
-          display: "flex",
-          justifyContent: "flex-end",
-          flexDirection: "column",
-          width: "48px",
-          ml: 1,
-        }}
-      >
-        {side === "right" && <Avatar />}
       </Box>
     </Box>
   )
