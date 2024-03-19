c5080730b43a9d30538adce28f559e6de0e10527
Ian Eaves <ian.k.eaves@gmail.com>
2024-03-19 17:50:44 -0500

    updated process_server

--- grai-server/app/api/order.py
+++ grai-server/app/api/order.py
@@ -8,6 +8,7 @@ from strawberry_django.ordering import process_order
 
 def apply_order(queryset: QuerySet, order: Optional[StrawberryField] = strawberry.UNSET):
     if order:
-        queryset = queryset.order_by(*process_order(order))
+        queryset, args = process_order(order, info=None, queryset=queryset)
+    # queryset = queryset.order_by(*generate_order_args(order))
 
     return queryset
