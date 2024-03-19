2333d7d8fe4098f3ee7c5335babcc544d6025b52
Ian Eaves <ian.k.eaves@gmail.com>
2024-03-19 17:58:48 -0500

    package-updates and bump version fix (#867)
    * package-updates and bump version fix
    
    * strawberry deprecation
    
    * updated process_server

--- grai-server/app/api/order.py
+++ grai-server/app/api/order.py
@@ -3,11 +3,12 @@ from typing import Optional
 import strawberry
 from django.db.models.query import QuerySet
 from strawberry.field import StrawberryField
-from strawberry_django.ordering import generate_order_args
+from strawberry_django.ordering import process_order
 
 
 def apply_order(queryset: QuerySet, order: Optional[StrawberryField] = strawberry.UNSET):
     if order:
-        queryset = queryset.order_by(*generate_order_args(order))
+        queryset, args = process_order(order, info=None, queryset=queryset)
+    # queryset = queryset.order_by(*generate_order_args(order))
 
     return queryset
