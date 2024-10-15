ad7f3096336e433b9e703ddf7b924c0f799e9bad
Ian Eaves <ian.k.eaves@gmail.com>
2024-10-14 19:09:26 -0500

    fix change to sqlserver sqlcmd location

--- grai-integrations/source-mssql/data/init-db.sh
+++ grai-integrations/source-mssql/data/init-db.sh
@@ -6,9 +6,10 @@ PORT="${DB_PORT:-"1433"}"
 MSSQL_SA_PASSWORD="${MSSQL_SA_PASSWORD:-"GraiGraiGr4i"}"
 SERVER="tcp:$HOST,$PORT"
 RETRY_LIMIT=${RETRY_LIMIT:-30}
+SQL_CMD_DIR=${SQL_CMD_DIR-"/opt/mssql-tools18/bin"}
 
 
-/opt/mssql-tools/bin/sqlcmd -S $SERVER -U sa -P $MSSQL_SA_PASSWORD -Q 'SELECT 1' -b -o /dev/null
+${SQL_CMD_DIR}/sqlcmd -S $SERVER -U sa -P $MSSQL_SA_PASSWORD -Q 'SELECT 1' -No -b -o /dev/null
 DBSTATUS=$?
 
 if [[ $DBSTATUS -ne 0 ]]; then
@@ -20,7 +21,7 @@ i=1
 while [[ $DBSTATUS -ne 0 ]] && [[ $i -lt $RETRY_LIMIT ]]; do
   i=$((i + 1))
 
-  /opt/mssql-tools/bin/sqlcmd -S $SERVER -U sa -P $MSSQL_SA_PASSWORD -Q 'SELECT 1' -b -o /dev/null
+  ${SQL_CMD_DIR}/sqlcmd -S $SERVER -U sa -P $MSSQL_SA_PASSWORD -Q 'SELECT 1' -No -b -o /dev/null
   DBSTATUS=$?
 
   if [[ $DBSTATUS -ne 0 ]]; then
@@ -39,5 +40,5 @@ echo "Database ready"
 for file in $(find $SCRIPT_DIR -type f -name '*.sql' | sort)
    do
      echo "Executing init script $file"
-     /opt/mssql-tools/bin/sqlcmd -U sa -P $MSSQL_SA_PASSWORD -l 30 -e -i $file
+     ${SQL_CMD_DIR}/sqlcmd -U sa -P $MSSQL_SA_PASSWORD -l 30 -e -i $file -No
    done
