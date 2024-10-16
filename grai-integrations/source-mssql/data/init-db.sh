24ba3d4746037c97a0b8df415e9d7c748efa4ab0
Ian Eaves <ian.k.eaves@gmail.com>
2024-10-16 16:28:42 -0500

    mssql timeout

--- grai-integrations/source-mssql/data/init-db.sh
+++ grai-integrations/source-mssql/data/init-db.sh
@@ -7,9 +7,12 @@ MSSQL_SA_PASSWORD="${MSSQL_SA_PASSWORD:-"GraiGraiGr4i"}"
 SERVER="tcp:$HOST,$PORT"
 RETRY_LIMIT=${RETRY_LIMIT:-60}
 SQL_CMD_DIR=${SQL_CMD_DIR-"/opt/mssql-tools18/bin"}
+SQL_CMD="sqlcmd -S $SERVER -U sa -P $MSSQL_SA_PASSWORD -No "
 
+export PATH=$PATH:$SQL_CMD_DIR
 
-${SQL_CMD_DIR}/sqlcmd -S $SERVER -U sa -P $MSSQL_SA_PASSWORD -Q 'SELECT 1' -No -b -o /dev/null
+${SQL_CMD} -Q 'SELECT 1' -b
+#-o /dev/null
 DBSTATUS=$?
 
 if [[ $DBSTATUS -ne 0 ]]; then
@@ -21,7 +24,8 @@ i=1
 while [[ $DBSTATUS -ne 0 ]] && [[ $i -lt $RETRY_LIMIT ]]; do
   i=$((i + 1))
 
-  ${SQL_CMD_DIR}/sqlcmd -S $SERVER -U sa -P $MSSQL_SA_PASSWORD -Q 'SELECT 1' -No -b -o /dev/null
+  ${SQL_CMD} -Q 'SELECT 1' -b
+  #-o /dev/null
   DBSTATUS=$?
 
   if [[ $DBSTATUS -ne 0 ]]; then
@@ -40,5 +44,5 @@ echo "Database ready"
 for file in $(find $SCRIPT_DIR -type f -name '*.sql' | sort)
    do
      echo "Executing init script $file"
-     ${SQL_CMD_DIR}/sqlcmd -U sa -P $MSSQL_SA_PASSWORD -l 30 -e -i $file -No
+     ${SQL_CMD} -l 30 -e -i $file
    done
