6fc1816db08031490e6e0d8dfd7d50afefcdb247
Ian Eaves <ian.k.eaves@gmail.com>
2024-10-16 19:29:34 -0500

    crazy-pills

--- grai-integrations/source-mssql/data/init-db.sh
+++ grai-integrations/source-mssql/data/init-db.sh
@@ -10,7 +10,8 @@ SQL_CMD_DIR=${SQL_CMD_DIR-"/opt/mssql-tools18/bin"}
 export PATH=$PATH:$SQL_CMD_DIR
 AUTH_ARG=${AUTH_ARG:-"-No"}
 SQL_CMD="sqlcmd -S $SERVER -U sa -P $MSSQL_SA_PASSWORD $AUTH_ARG"
-
+echo $SQL_CMD
+SQL_CMD="sqlcmd -S $SERVER -U sa -P $MSSQL_SA_PASSWORD"
 $SQL_CMD -Q 'SELECT 1' -b -o /dev/null
 DBSTATUS=$?
 if [[ $DBSTATUS -ne 0 ]]; then
